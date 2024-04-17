13b2172a6bf202ef82b3ba8dd6de552b31377eec
Dom Vournias <domvournias@gmail.com>
2024-04-17 19:44:40 +0300

    ServicePackages - Components

---
+++ src/components/dashboard/section/ServicePackages.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Packages from "./ServicePackages/Packages";
+
+export default function ServicePackages() {
+  return (
+    <>
+      <div className="ps-widget bgc-white bdrs12 p30 mb30 overflow-hidden position-relative">
+        <div className="bdrb1 ">
+          <h3 className="list-title">Πακέτα</h3>
+        </div>
+        <Packages />
+      </div>
+    </>
+  );
+}
