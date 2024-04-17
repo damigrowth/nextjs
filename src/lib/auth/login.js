00d7b139ebbd6e2f51c41fb26f25a7d4962cafc2
Dom Vournias <domvournias@gmail.com>
2024-04-17 18:54:31 +0300

    Login - Actions

---
+++ src/lib/auth/login.js
@@ -0,0 +1,59 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const formSchema = z.object({
+  identifier: z.string().min(2).max(50),
+  password: z.string().min(6).max(100),
+});
+
+export async function login(prevState, formData) {
+  const STRAPI_URL = process.env.STRAPI_API_URL;
+  const STRAPI_TOKEN = process.env.STRAPI_API_TOKEN;
+
+  if (!STRAPI_URL) throw new Error("Missing STRAPI_URL environment variable.");
+
+  const url = `${STRAPI_URL}/auth/local`;
+
+  const validatedFields = formSchema.safeParse({
+    identifier: formData.get("identifier"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Login.",
+    };
+  }
+
+  const { identifier, password } = validatedFields.data;
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        identifier,
+        password,
+      }),
+      cache: "no-cache",
+    });
+
+    const data = await response.json();
+
+    if (!response.ok && data.error)
+      return { ...prevState, message: data.error.message, errors: null };
+    if (response.ok && data.jwt) {
+      cookies().set("jwt", data.jwt);
+    }
+  } catch (error) {
+    console.error("Login error:", error);
+    return { error: "Server error. Please try again later." };
+  }
+  redirect("/");
+}
