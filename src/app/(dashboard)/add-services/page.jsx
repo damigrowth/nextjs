f28340e4f1c4baaf35b1ec8bacbe18e59072a781
Dom Vournias <domvournias@gmail.com>
2024-04-17 19:22:29 +0300

    Add Services - Dashboard - Pages

--- src/app/(dashboard)/add-services/page.jsx
+++ src/app/(dashboard)/add-services/page.jsx
@@ -4,14 +4,13 @@ import AddServiceInfo from "@/components/dashboard/section/AddServiceInfo";
 import MobileNavigation2 from "@/components/header/MobileNavigation2";
 
 export const metadata = {
-  title: "Freeio - Freelance Marketplace React/Next Js Template | Add Service",
+  title: "Doulitsa",
 };
 
 export default function page() {
   return (
     <>
-    
-    <MobileNavigation2 />
+      <MobileNavigation2 />
       <DashboardLayout>
         <AddServiceInfo />
       </DashboardLayout>
--- src/app/(dashboard)/dashboard/page.jsx
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -1,17 +1,20 @@
-import DashboardLayout from "@/components/dashboard/DashboardLayout";
 import DashboardInfo from "@/components/dashboard/section/DashboardInfo";
-
+import DashboardLayout from "@/components/dashboard/DashboardLayout";
 import MobileNavigation2 from "@/components/header/MobileNavigation2";
+import { cookies } from "next/headers";
+import { getUser } from "@/lib/user/user";
 
 export const metadata = {
   title: "Freeio - Freelance Marketplace React/Next Js Template | Dashboard",
 };
 
-export default function page() {
+export default async function page() {
+  const user = await getUser();
+
+  // console.log("USER - Dashboard ==>", user);
   return (
     <>
-    
-    <MobileNavigation2 />
+      <MobileNavigation2 />
       <DashboardLayout>
         <DashboardInfo />
       </DashboardLayout>
