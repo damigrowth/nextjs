c167dfafb72b9313e6ebbda740ebb5e07fde7f9d
Dom Vournias <domvournias@gmail.com>
2024-04-17 19:42:15 +0300

    ExtraService - Components

--- src/components/dashboard/section/ExtraService.jsx
+++ src/components/dashboard/section/ExtraService.jsx
@@ -4,8 +4,8 @@ export default function ExtraService() {
   return (
     <>
       <div className="ps-widget bgc-white bdrs12 p30 mb30 overflow-hidden position-relative">
-        <div className="bdrb1 pb15 mb25">
-          <h5 className="list-title">Add extra services</h5>
+        <div className="bdrb1 ">
+          <h3 className="list-title">Πρόσθετα</h3>
         </div>
         <div className="col-xl-8">
           <ServiceDetailExtra1 />
