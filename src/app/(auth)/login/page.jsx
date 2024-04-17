7e7ebb332d7d6090dfa97f6a14a3227183bc6000
Dom Vournias <domvournias@gmail.com>
2024-04-17 18:39:15 +0300

    Login page - Auth

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import LoginForm from "@/components/forms/LoginForm";
 
 export default function page() {
   return (
@@ -11,10 +12,9 @@ export default function page() {
               data-wow-delay="300ms"
             >
               <div className="main-title text-center">
-                <h2 className="title">Log In</h2>
+                <h2 className="title">Είσοδος</h2>
                 <p className="paragraph">
-                  Give your visitor a smooth online experience with a solid UX
-                  design
+                  Κάνε σύνδεση ή εγγραφή με έναν από τους παρακάτω τρόπους.
                 </p>
               </div>
             </div>
@@ -23,68 +23,15 @@ export default function page() {
             <div className="col-xl-6 mx-auto">
               <div className="log-reg-form search-modal form-style1 bgc-white p50 p30-sm default-box-shadow1 bdrs12">
                 <div className="mb30">
-                  <h4>We're glad to see you again!</h4>
+                  <h4>Συνέχεια με τον λογαριασμό σου</h4>
                   <p className="text">
-                    Don't have an account?{" "}
+                    Δεν έχεις λογαριασμό?{" "}
                     <Link href="/register" className="text-thm">
-                      Sign Up!
+                      Εγγραφή!
                     </Link>
                   </p>
                 </div>
-                <div className="mb20">
-                  <label className="form-label fw600 dark-color">
-                    Email Address
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="alitfn58@gmail.com"
-                  />
-                </div>
-                <div className="mb15">
-                  <label className="form-label fw600 dark-color">
-                    Password
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="*******"
-                  />
-                </div>
-                <div className="checkbox-style1 d-block d-sm-flex align-items-center justify-content-between mb20">
-                  <label className="custom_checkbox fz14 ff-heading">
-                    Remember me
-                    <input type="checkbox" defaultChecked="checked" />
-                    <span className="checkmark" />
-                  </label>
-                  <a className="fz14 ff-heading">Lost your password?</a>
-                </div>
-                <div className="d-grid mb20">
-                  <button className="ud-btn btn-thm" type="button">
-                    Log In <i className="fal fa-arrow-right-long" />
-                  </button>
-                </div>
-                <div className="hr_content mb20">
-                  <hr />
-                  <span className="hr_top_text">OR</span>
-                </div>
-                <div className="d-md-flex justify-content-between">
-                  <button
-                    className="ud-btn btn-fb fz14 fw400 mb-2 mb-md-0"
-                    type="button"
-                  >
-                    <i className="fab fa-facebook-f pr10" /> Continue Facebook
-                  </button>
-                  <button
-                    className="ud-btn btn-google fz14 fw400 mb-2 mb-md-0"
-                    type="button"
-                  >
-                    <i className="fab fa-google" /> Continue Google
-                  </button>
-                  <button className="ud-btn btn-apple fz14 fw400" type="button">
-                    <i className="fab fa-apple" /> Continue Apple
-                  </button>
-                </div>
+                <LoginForm />
               </div>
             </div>
           </div>
