b4ce6814ff1dd2c35c3d001e614a6e80cd6ee023
Dom Vournias <domvournias@gmail.com>
2024-04-17 19:05:03 +0300

    Protected HOCs - Auth - Components

---
+++ src/components/auth/Public.jsx
@@ -0,0 +1,7 @@
+import { isAuthenticated } from "@/lib/auth/authenticated";
+
+export default async function Public({ children }) {
+  const authenticated = await isAuthenticated();
+
+  return authenticated === false ? children : null;
+}
