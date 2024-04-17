5e87b93be8033d980fafe749c39da4e115b806ea
Dom Vournias <domvournias@gmail.com>
2024-04-17 19:01:14 +0300

    Middleware

---
+++ src/middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  const cookie = request.cookies.get("jwt");
+
+  const currentPath = request.nextUrl.pathname;
+
+  if (currentPath === "/dashboard" && cookie === undefined) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (currentPath === "/dashboard/invoices" && cookie === undefined) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (currentPath === "/dashboard/customers" && cookie === undefined) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
