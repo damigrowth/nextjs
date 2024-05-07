5a5bf32cc03624d67a16a2e3a989098f46365eb0
Dom Vournias <domvournias@gmail.com>
2024-05-07 18:20:51 +0300

    StepsStore - update

--- src/store/service/stepsStore.js
+++ src/store/service/stepsStore.js
@@ -1,35 +1,65 @@
-const useStepsStore = (set) => ({
-  step: "info",
-  steps: {
-    info: {
-      previous: null,
-      next: "packages",
-    },
-    packages: {
-      previous: "info",
-      next: "addons",
-    },
-    addons: {
-      previous: "packages",
-      next: "faq",
-    },
-    faq: {
-      previous: "addons",
-      next: "gallery",
-    },
-    gallery: {
-      previous: "faq",
-      next: null,
-    },
+const initialStep = "info";
+const initialFixedStepsTypeState = {
+  info: {
+    previous: null,
+    next: "addons",
   },
+  addons: {
+    previous: "info",
+    next: "faq",
+  },
+  faq: {
+    previous: "addons",
+    next: "gallery",
+  },
+  gallery: {
+    previous: "faq",
+    next: null,
+  },
+};
+const initialPackagesStepsTypeState = {
+  info: {
+    previous: null,
+    next: "packages",
+  },
+  packages: {
+    previous: "info",
+    next: "addons",
+  },
+  addons: {
+    previous: "packages",
+    next: "faq",
+  },
+  faq: {
+    previous: "addons",
+    next: "gallery",
+  },
+  gallery: {
+    previous: "faq",
+    next: null,
+  },
+};
+
+const useStepsStore = (set) => ({
+  step: initialStep,
+  steps: initialFixedStepsTypeState,
   setStep: (currentStep) => set(() => ({ step: currentStep })),
-  //   setStep: (step) =>
-  //     set((state) => {
-  //       if (state.saved.info === false) {
-  //         console.log("not saved");
-  //       }
-  //       return {};
-  //     }),
+  handleStepsTypeChange: (fixed) =>
+    set((state) => {
+      if (fixed === false || fixed === null) {
+        return {
+          ...state,
+          steps: initialFixedStepsTypeState,
+        };
+      }
+
+      if (fixed === true) {
+        return {
+          ...state,
+          steps: initialPackagesStepsTypeState,
+        };
+      }
+    }),
 });
 
 export default useStepsStore;
