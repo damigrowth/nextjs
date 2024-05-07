09694110f12ecd324948d5c7ee4dc5de0a6259c6
Dom Vournias <domvournias@gmail.com>
2024-05-07 18:30:21 +0300

    Updated AddServiceInfo

--- src/components/dashboard/section/AddServiceInfo.jsx
+++ src/components/dashboard/section/AddServiceInfo.jsx
@@ -1,11 +1,16 @@
 import AddServiceForm from "@/components/forms/AddServiceForm";
 import DashboardNavigation from "../header/DashboardNavigation";
-import { fetchServiceForm } from "@/lib/service/data";
+import { fetchModel } from "@/lib/models/model";
+import { CATEGORIES, LOCATIONS, LOCATIONS_SEARCH, SKILLS } from "@/lib/queries";
 
-export default async function AddServiceInfo() {
-  const { categories, skills, cities } = await fetchServiceForm();
+export default async function AddServiceInfo({ locationsSearchQuery }) {
+  const { categories } = await fetchModel("categories", CATEGORIES);
+  const { skills } = await fetchModel("skills", SKILLS);
+  const { locations } = await fetchModel(
+    "locations",
+    LOCATIONS_SEARCH(locationsSearchQuery)
+  );
 
-  // console.log(categories, skills, cities);
   return (
     <>
       <div className="dashboard__content hover-bgc-color">
@@ -17,7 +22,7 @@ export default async function AddServiceInfo() {
             <AddServiceForm
               categories={categories}
               skills={skills}
-              cities={cities}
+              locations={locations}
             />
           </div>
         </div>
