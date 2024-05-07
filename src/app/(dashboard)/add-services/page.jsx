bccecdb5d75e4e14d24218eb533f2e217e087cd1
Dom Vournias <domvournias@gmail.com>
2024-05-07 18:25:37 +0300

    Added search params and locations

--- src/app/(dashboard)/add-services/page.jsx
+++ src/app/(dashboard)/add-services/page.jsx
@@ -2,17 +2,22 @@ import DashboardLayout from "@/components/dashboard/DashboardLayout";
 import AddServiceInfo from "@/components/dashboard/section/AddServiceInfo";
 
 import MobileNavigation2 from "@/components/header/MobileNavigation2";
+import { fetchModel } from "@/lib/models/model";
+import { CATEGORIES, LOCATIONS_SEARCH, SKILLS } from "@/lib/queries";
 
 export const metadata = {
   title: "Doulitsa",
 };
 
-export default function page() {
+export default async function page({ searchParams }) {
+  const locationsSearchQuery = searchParams?.location || "";
+
+  console.log("searchParams:", searchParams);
   return (
     <>
       <MobileNavigation2 />
       <DashboardLayout>
-        <AddServiceInfo />
+        <AddServiceInfo locationsSearchQuery={locationsSearchQuery} />
       </DashboardLayout>
     </>
   );
