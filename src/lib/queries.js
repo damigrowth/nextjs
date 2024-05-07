9302f5aaba08529852b907558748a4a2bc64540e
Dom Vournias <domvournias@gmail.com>
2024-05-07 18:24:19 +0300

    Added Locations search function Queries

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -2,3 +2,7 @@ export const CATEGORIES = "categories?fields[0]=title";
 export const SKILLS = "skills?fields[0]=title";
 export const LOCATIONS =
   "locations/?fields[0]=zipcode&fields[1]=area&fields[2]=county";
+export const LOCATIONS_SEARCH = (query) => {
+  const url = `locations?fields[0]=zipcode&fields[1]=area&fields[2]=county&filters[area][$contains]=${query}`;
+  return url;
+};
