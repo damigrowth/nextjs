0ca5dfba1d16c28c7ecba55d6723d3d9f51deaac
Dom Vournias <domvournias@gmail.com>
2024-05-07 18:23:00 +0300

    Strapi env vars

---
+++ src/lib/strapi.js
@@ -0,0 +1,12 @@
+// Strapi Url and Token
+export const STRAPI_URL = process.env.STRAPI_API_URL;
+export const STRAPI_TOKEN = process.env.STRAPI_API_TOKEN;
+
+export const validateEnvVars = () => {
+  // Check for required environment variables
+  if (!STRAPI_URL || !STRAPI_TOKEN) {
+    throw new Error(
+      "Missing STRAPI_API_URL or STRAPI_API_TOKEN environment variable."
+    );
+  }
+};
