a66cf5f9b4c425b0f4baea203ecc9456cd32bc6e
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-05-19 19:43:34 +0300

    Updated isAuthenticated function

--- src/lib/auth/authenticated.js
+++ src/lib/auth/authenticated.js
@@ -1,11 +1,10 @@
-"use server";
-
 import { cookies } from "next/headers";
 
 export async function isAuthenticated() {
   try {
     const token = cookies().get("jwt")?.value;
-    return token === undefined ? false : true;
+    const authenticated = token === undefined ? false : true;
+    return { authenticated };
   } catch (error) {
     console.error("Get Cookie error:", error);
     return { error: "Could not get cookie." };
