4c1202700b6a5741171985755ecae2b730e219c9
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-05-19 19:42:37 +0300

    Added Maintenance mode and refactored middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,28 +1,46 @@
 import { NextResponse } from "next/server";
+import { getMaintenanceStatus } from "./lib/maintenance/maintenance";
 
-export function middleware(request) {
+export async function middleware(request) {
   const cookie = request.cookies.get("jwt");
-
   const currentPath = request.nextUrl.pathname;
+  const { isUnderMaintenance } = await getMaintenanceStatus();
 
-  if (currentPath === "/dashboard" && cookie === undefined) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // List of paths that should be publicly accessible
+  const maintenancePublicPaths = [
+    "/maintenance",
+    "/login",
+    "/favicon.ico",
+    "/_next",
+    "/static",
+    "/images",
+    "/styles",
+    "/scripts",
+  ];
 
-  if (currentPath === "/dashboard/invoices" && cookie === undefined) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  const protectedPaths = [
+    "/dashboard",
+    "/dashboard/invoices",
+    "/dashboard/customers",
+    "/add-services",
+    "/manage-services",
+  ];
 
-  if (currentPath === "/dashboard/customers" && cookie === undefined) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  if (isUnderMaintenance) {
+    // Check if the current path starts with any of the public paths
+    const isPublicPath = maintenancePublicPaths.some((path) =>
+      currentPath.startsWith(path)
+    );
 
-  if (currentPath === "/add-services" && cookie === undefined) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  if (currentPath === "/manage-services" && cookie === undefined) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    // If the path is not public and the cookie is missing, redirect to the login page
+    if (!isPublicPath && !cookie) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  } else {
+    // Redirect to the login page if trying to access protected paths without a cookie
+    if (protectedPaths.includes(currentPath) && cookie === undefined) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
   }
 
   return NextResponse.next();
