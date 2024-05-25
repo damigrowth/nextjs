df000ddef5841379c808c79bc48d9031d4c467d0
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-05-25 16:20:11 +0300

    formatDate

---
+++ src/utils/formatDate.js
@@ -0,0 +1,13 @@
+// utils/formatDate.js
+import { format } from "date-fns";
+
+/**
+ * Formats a given date string to 'dd MMMM yyyy'.
+ * @param {string} dateStr - The date string to format.
+ * @returns {string} - The formatted date string.
+ */
+export const formatDate = (dateStr, type) => {
+  const date = new Date(dateStr);
+  const formattedDate = format(date, type);
+  return { formattedDate };
+};
