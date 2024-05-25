d6a642befc2b3ee2e94a5ae6245b6300ede86e50
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-05-25 16:23:29 +0300

    ServiceDetailComment1

--- src/components/element/ServiceDetailComment1.jsx
+++ src/components/element/ServiceDetailComment1.jsx
@@ -1,66 +1,49 @@
+"use client";
+
+import { useState } from "react";
+import ReactStars from "react-stars";
+
 export default function ServiceDetailComment1() {
+  const [rating, setRating] = useState(1);
+
+  const handleRatingChange = (value) => {
+    setRating(value);
+    console.log(value);
+  };
   return (
     <>
       <div className="bsp_reveiw_wrt mb20">
-        <h6 className="fz17">Add a Review</h6>
+        <h6 className="fz17">Πρόσθεσε Κριτική</h6>
         <p className="text">
-          Your email address will not be published. Required fields are marked *
+          Το email σας δεν θα δημοσιευτεί, μόνο το όνομα και το επίθετο.
         </p>
-        <h6>Your rating of this product</h6>
-        <div className="d-flex">
-          <i className="fas fa-star review-color" />
-          <i className="far fa-star review-color ms-2" />
-          <i className="far fa-star review-color ms-2" />
-          <i className="far fa-star review-color ms-2" />
-          <i className="far fa-star review-color ms-2" />
-        </div>
+        <h6>Βαθμολογία</h6>
+        <ReactStars
+          count={5}
+          value={rating}
+          half={false}
+          onChange={handleRatingChange}
+          size={24}
+          color2={"#ffd700"}
+        />
         <form className="comments_form mt30 mb30-md">
           <div className="row">
             <div className="col-md-12">
               <div className="mb-4">
                 <label className="fw500 fz16 ff-heading dark-color mb-2">
-                  Comment
+                  Κριτική
                 </label>
                 <textarea
                   className="pt15"
                   rows={6}
-                  placeholder="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
+                  placeholder="Η υπηρεσία ήταν εξαιρετική!"
                 />
               </div>
             </div>
-            <div className="col-md-6">
-              <div className="mb20">
-                <label className="fw500 ff-heading dark-color mb-2">Name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Ali Tufan"
-                />
-              </div>
-            </div>
-            <div className="col-md-6">
-              <div className="mb20">
-                <label className="fw500 ff-heading dark-color mb-2">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="creativelayers088"
-                />
-              </div>
-            </div>
+
             <div className="col-md-12">
-              <div className="checkbox-style1 d-block d-sm-flex align-items-center justify-content-between mb20">
-                <label className="custom_checkbox fz15 ff-heading">
-                  Save my name, email, and website in this browser for the next
-                  time I comment.
-                  <input type="checkbox" />
-                  <span className="checkmark" />
-                </label>
-              </div>
               <a className="ud-btn btn-thm">
-                Send
+                Αποστολή
                 <i className="fal fa-arrow-right-long" />
               </a>
             </div>
