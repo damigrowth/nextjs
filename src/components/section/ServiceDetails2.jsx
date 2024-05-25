f40b86ca49e29876377be93557968b13f4e374f4
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-05-25 16:25:11 +0300

    ServiceDetailFaq1

--- src/components/section/ServiceDetails2.jsx
+++ src/components/section/ServiceDetails2.jsx
@@ -227,7 +227,7 @@ export default function ServiceDetail2() {
                     </div>
                     <hr className="opacity-100 mb60" />
                     <h4>Frequently Asked Questions</h4>
-                    <ServiceDetailFaq1 />
+                    {/* <ServiceDetailFaq1 /> */}
                     <hr className="opacity-100 mb60" />
                     <h4>Add Extra Services</h4>
                     <ServiceDetailExtra1 />
