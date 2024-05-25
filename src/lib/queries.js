7414aa17512208590b4190ab7f6a6a48d43152e3
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-05-25 16:21:46 +0300

    queries

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -38,3 +38,36 @@ export const ZIPCODES_BY_AREA = (areaId) => {
   const url = `areas/${areaId}/?fields[0]=name&populate[zipcodes][fields][0]=name`;
   return url;
 };
+
+export const REVIEW = (reviewId) => {
+  const url = `reviews/${reviewId}/?populate[user][fields]=id&populate[type][fields]=name&populate[status][fields]=type&populate[service][fields]=id&populate[likes][fields]=id&fields[0]=comment&fields[1]=rating`;
+};
+
+export const SERVICE = (serviceId) => {
+  const url = `services/${serviceId}?populate[freelancer][fields]=*&populate[category][fields][0]=title&populate[area][fields]=name&populate[skills][fields][0]=title&populate[packages][populate][0]=features&populate[addons][fields]=*&populate[faq][fields]=*&populate[media][fields]=formats&populate[status][fields]=type&populate[rating_global][fields]=name&populate[rating_global][fields]=grade`;
+  return url;
+};
+
+export const REVIEWS_BY_SERVICE = (serviceId) => {
+  const url = `reviews?filters[service][id][$eq]=${serviceId}&publicationState=live&populate[user][fields]=displayName&populate[type][fields]=name&populate[status][fields]=type&populate[likes][fields]=id&populate[dislikes][fields]=id`;
+
+  return url;
+};
+export const REVIEW_REACT = (type, reviewId) => {
+  const url = `reviews/${reviewId}/?populate[${type}s][fields]=id&fields=id`;
+  return url;
+};
+
+export const RATINGS = `ratings?sort[0]=id:desc&fields[0]=name&fields[1]=grade`;
+
+export const RATING_SERVICES_COUNT = (ratingId) => {
+  const url = `ratings/${ratingId}?fields[0]=id&populate[services][fields]=id`;
+  return url;
+};
+
+export const SERVICE_RATING_UPDATE = (serviceId) => {
+  const url = `services/${serviceId}?fields[0]=rating&populate[rating_global][fields]=name&populate[rating_global][fields]=grade&populate[rating_global][fields]=id`;
+  return url;
+};
+
+export const POST_REVIEW = `reviews?populate[status][fields]=type`;
