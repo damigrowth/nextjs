1d3ae8d127821cf29f35e92606594018e0edf619
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-05-25 16:25:23 +0300

    ReviewSuccess

---
+++ src/components/dashboard/section/ReviewSuccess/ReviewSuccess.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function ReviewSuccess({ id }) {
+  return (
+    <div className="ps-widget bgc-white bdrs12 p30 mb30 overflow-hidden position-relative">
+      <div className="success-container">
+        <div className="success-icon bgc-thm">
+          <i className="flaticon-success vam fz40 text-white " />
+        </div>
+        <h3 className="list-title text-center">Επιτυχής αποστολή κριτικής!</h3>
+        <p className="text-center">
+          Ευχαριστούμε για την κριτική σας με κωδικό <strong>#{id}</strong>.{" "}
+          <br />
+          Σύντομα θα γίνει η δημοσίευση της αφού ολοκληρωθεί η διαδικασία
+          ελέγχου της.
+        </p>
+      </div>
+    </div>
+  );
+}
