8ad6be5bc322303982c84e98c61ee8a8cfd31d8c
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-05-25 16:21:00 +0300

    service query

--- src/lib/service/index.js
+++ src/lib/service/index.js
@@ -5,7 +5,7 @@ export async function getService(serviceId) {
 
   if (!STRAPI_URL) throw new Error("Missing STRAPI_URL environment variable.");
 
-  const url = `services/${serviceId}?populate[freelancer][fields]=*&populate[category][fields][0]=title&populate[city][fields][0]=title&populate[skills][fields][0]=title&populate[packages][populate][0]=features&populate[addons][fields]=*&populate[faq][fields]=*`;
+  const url = `services/${serviceId}?populate[freelancer][fields]=*&populate[category][fields][0]=title&populate[city][fields][0]=title&populate[skills][fields][0]=title&populate[packages][populate][0]=features&populate[addons][fields]=*&populate[faq][fields]=*&populate[media][fields]=formats&populate[status][fields]=type&populate[reviews][populate][user][fields]=id&populate[reviews][populate][type][fields]=name&populate[reviews][populate][status][fields]=type&populate[reviews][populate][likes][fields]=id`;
 
   const data = await getData(url);
 
