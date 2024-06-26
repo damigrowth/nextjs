16f930f67c92d2fe90bf2acbbb6726f9db9e7133
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-06-26 20:44:56 +0300

    Update page.jsx

--- src/app/(service)/service/[slug]/page.jsx
+++ src/app/(service)/service/[slug]/page.jsx
@@ -5,12 +5,19 @@ import TabSection1 from "@/components/section/TabSection1";
 import { redirect } from "next/navigation";
 import { fetchModel } from "@/lib/models/model";
 import { VIEWS_BY_SERVICE_USER } from "@/lib/queries";
-import SingleService from "@/components/dashboard/section/SingleService";
+import SingleService from "@/components/ui/SingleService";
 import { postData, putData } from "@/lib/api";
 import { getUserId } from "@/lib/user/user";
 import { truncateText } from "@/utils/truncateText";
 import { getData } from "@/lib/client/operations";
 import { RATINGS, SERVICE_BY_SLUG } from "@/lib/graphql/queries";
+import {
+  getAllReviewsRatingsByService,
+  getReviewsByService,
+  getServiceBySlug,
+} from "@/lib/service/service";
+import { getRatings } from "@/lib/rating/get";
+import { getReviewsByFreelancer } from "@/lib/freelancer/freelancer";
 
 // Dynamic SEO
 export async function generateMetadata({ params }) {
@@ -44,51 +51,62 @@ export async function generateMetadata({ params }) {
   }
 }
 
-export default async function page({ params }) {
+export default async function page({ params, searchParams }) {
   const serviceSlug = params.slug;
-  const userId = await getUserId();
 
-  const { services } = await getData(SERVICE_BY_SLUG, { slug: serviceSlug });
+  const { service, uid } = await getServiceBySlug(serviceSlug);
 
-  const serviceId = Number(services.data[0]?.id);
-  const service = services.data[0].attributes;
-
-  if (service === undefined || serviceId === undefined) {
+  if (!service) {
     redirect("/not-found");
   } else {
-    // Get the ratings and reviews
-    // const { ratings } = await fetchModel("ratings", RATINGS);
-    // const { reviews } = await fetchModel(
-    //   "reviews",
-    //   REVIEWS_BY_SERVICE(serviceId)
-    // );
-    // console.log("REVIEWS ORIGINAL", reviews);
+    let reviewsPage = parseInt(searchParams.reviews, 10);
+    reviewsPage = !reviewsPage || reviewsPage < 1 ? 1 : reviewsPage;
+    const reviewsPageSize = reviewsPage * 3;
 
-    const reviews = service.reviews?.data;
-    const ratingsData = await getData(RATINGS);
-    const ratings = ratingsData.ratings?.data;
-    // console.log("RATINGS", ratings);
-    // console.log("REVIEWS", reviews);
+    const freelancerId = Number(service.freelancer.data.id);
 
-    if (serviceId && userId) {
-      // Get current views of service based on service id and user id
-      const { views } = await fetchModel(
-        "views",
-        VIEWS_BY_SERVICE_USER(serviceId, userId)
-      );
+    // const userId = await getUserId();
+    const ratings = await getRatings();
 
-      if (views) {
-        // Check if the length of the filtered array is not empty
-        if (views.length === 0) {
-          const newView = {
-            user: userId,
-            service: serviceId,
-          };
+    const { reviews, reviewsMeta } = await getReviewsByService(
+      uid,
+      1,
+      reviewsPageSize
+    );
 
-          await postData("views", newView);
-        }
-      }
-    }
+    const { allReviewsRatings } = await getAllReviewsRatingsByService(
+      uid,
+      1000
+    );
+
+    const { reviewsMeta: freelancerReviewsMeta } = await getReviewsByFreelancer(
+      freelancerId,
+      1,
+      1
+    );
+
+    const totalFreelancerReviews = freelancerReviewsMeta.total;
+
+    // if (serviceId && userId) {
+    //   // Get current views of service based on service id and user id
+    //   const { views } = await fetchModel(
+
+    //     "views",
+    //     VIEWS_BY_SERVICE_USER(serviceId, userId)
+    //   );
+
+    //   if (views) {
+    //     // Check if the length of the filtered array is not empty
+    //     if (views.length === 0) {
+    //       const newView = {
+    //         user: userId,
+    //         service: serviceId,
+    //       };
+
+    //       await postData("views", newView);
+    //     }
+    //   }
+    // }
 
     return (
       <>
@@ -96,10 +114,14 @@ export default async function page({ params }) {
         <div className=" bgc-thm3">
           <Breadcumb3 path={["Home", "Services", "Design & Creative"]} />
           <SingleService
-            serviceId={serviceId}
+            serviceId={uid}
             service={service}
             reviews={reviews}
             ratings={ratings}
+            reviewsPage={reviewsPage}
+            reviewsMeta={reviewsMeta}
+            allReviewsRatings={allReviewsRatings}
+            totalFreelancerReviews={totalFreelancerReviews}
           />
           <PopulerService />
         </div>
