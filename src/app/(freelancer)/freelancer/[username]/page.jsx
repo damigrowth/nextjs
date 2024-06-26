3d214999a963193f81d48c1c1904e2a702a79a00
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-06-26 20:45:02 +0300

    Update page.jsx

--- src/app/(freelancer)/freelancer/[username]/page.jsx
+++ src/app/(freelancer)/freelancer/[username]/page.jsx
@@ -1,25 +1,73 @@
 import Breadcumb10 from "@/components/breadcumb/Breadcumb10";
-import FreelancerProfile from "@/components/profiles/freelancer/FreelancerProfile";
+import FreelancerProfile from "@/components/ui/profiles/freelancer/FreelancerProfile";
 import TabSection1 from "@/components/section/TabSection1";
-import { getData } from "@/lib/client/operations";
-import { FREELANCER_BY_USERNAME } from "@/lib/graphql/queries";
+import {
+  getAllReviewsRatingsByFreelancer,
+  getFeaturedServicesByFreelancer,
+  getFreelancerByUsername,
+  getReviewsByFreelancer,
+} from "@/lib/freelancer/freelancer";
+import { getRatings } from "@/lib/rating/get";
+import { inspect } from "@/utils/inspect";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   title:
     "Freeio - Freelance Marketplace React/Next Js Template | Freelancer Single",
 };
 
-export default async function page({ params }) {
+export default async function page({ params, searchParams }) {
   const username = params.username;
 
-  const { freelancers } = await getData(FREELANCER_BY_USERNAME(username));
+  const { freelancer, uid } = await getFreelancerByUsername(username);
 
-  // console.log("FREELANCER", freelancers);
-  return (
-    <>
-      <TabSection1 />
-      <Breadcumb10 path={["Home", "Services", "Design & Creative"]} />
-      <FreelancerProfile />
-    </>
-  );
+  if (!freelancer) {
+    redirect("/not-found");
+  } else {
+    let servicesPage = parseInt(searchParams.services, 10);
+    servicesPage = !servicesPage || servicesPage < 1 ? 1 : servicesPage;
+    const servicesPageSize = servicesPage * 3;
+
+    let reviewsPage = parseInt(searchParams.reviews, 10);
+    reviewsPage = !reviewsPage || reviewsPage < 1 ? 1 : reviewsPage;
+    const reviewsPageSize = reviewsPage * 3;
+
+    const ratings = await getRatings();
+
+    const { reviews, reviewsMeta } = await getReviewsByFreelancer(
+      uid,
+      1,
+      reviewsPageSize
+    );
+
+    const { allReviewsRatings } = await getAllReviewsRatingsByFreelancer(
+      uid,
+      1000
+    );
+
+    const { services, servicesMeta } = await getFeaturedServicesByFreelancer(
+      uid,
+      servicesPageSize
+    );
+
+    return (
+      <>
+        <TabSection1 />
+        <Breadcumb10 path={["Home", "Services", "Design & Creative"]} />
+        <FreelancerProfile
+          uid={uid}
+          freelancer={freelancer}
+          services={services}
+          servicesMeta={servicesMeta}
+          servicesPage={servicesPage}
+          ratings={ratings}
+          reviews={reviews}
+          reviewsMeta={reviewsMeta}
+          reviewsPage={reviewsPage}
+          allReviewsRatings={allReviewsRatings}
+        />
+      </>
+    );
+  }
+  // inspect(freelancer);
 }
