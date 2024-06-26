9696342ad518629eb53f4930cbe1b6b130d2b299
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-06-26 20:40:52 +0300

    Create Metrics.jsx

---
+++ src/components/ui/profiles/freelancer/Metrics.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+export default function Metrics({
+  type,
+  servicesTotal,
+  commencement,
+  verification,
+}) {
+  /// Get the current year
+  const currentYear = new Date().getFullYear();
+
+  // Calculate years of experience
+  const yearsOfExperience = currentYear - commencement;
+
+  const verified = verification === "Completed" ? true : false;
+
+  return (
+    <div className="row">
+      {type && (
+        <div className="col-sm-6 col-xl-3">
+          <div className="iconbox-style1 contact-style d-flex align-items-start mb30">
+            <div className="icon flex-shrink-0">
+              <span className="flaticon-briefcase" />
+            </div>
+            <div className="details">
+              <h5 className="title">{type?.label}</h5>
+              {/* <p className="mb-0 text">98%</p> */}
+            </div>
+          </div>
+        </div>
+      )}
+
+      <div className="col-sm-6 col-xl-3">
+        <div className="iconbox-style1 contact-style d-flex align-items-start mb30">
+          <div className="icon flex-shrink-0">
+            <span className="flaticon-page" />
+          </div>
+          <div className="details">
+            <h5 className="title">Υπηρεσίες</h5>
+            <p className="mb-0 text">{servicesTotal}</p>
+          </div>
+        </div>
+      </div>
+      {commencement && (
+        <div className="col-sm-6 col-xl-3">
+          <div className="iconbox-style1 contact-style d-flex align-items-start mb30">
+            <div className="icon flex-shrink-0">
+              <span className="flaticon-sand-clock" />
+            </div>
+            <div className="details">
+              <h5 className="title">Έτη Εμπειρίας</h5>
+              <p className="mb-0 text">{yearsOfExperience}</p>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <div className="col-sm-6 col-xl-3">
+        <div className="iconbox-style1 contact-style d-flex align-items-start mb30">
+          <div className="icon flex-shrink-0">
+            <span className="flaticon-success" />
+          </div>
+          <div className="details">
+            <h5 className="title">Πιστοποιημένος</h5>
+            <p className="mb-0 text">{verified ? "Ναι" : "Όχι"}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
