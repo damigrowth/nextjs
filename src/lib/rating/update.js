c8035d822743bd6fdad4ddfe005f2ef9b629d787
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-06-26 20:36:48 +0300

    Update update.js

--- src/lib/rating/update.js
+++ src/lib/rating/update.js
@@ -1,49 +1,99 @@
 "use server";
 
-import { getData, putData } from "../api";
-import { RATING_SERVICES_COUNT, SERVICE_RATING_UPDATE } from "../queries";
+import { putData } from "../client/operations";
+import { UPDATE_FREELANCER_RATING } from "../graphql/mutations";
+import { getRatingsModelCount } from "./get";
+
+// GET MODEL COUNT OF SPECIFIC RATING
+// const ratingModelCount = await getRatingsModelCount(
+//   type,
+//   rating_global.data.id
+// );
 
 // React to a Review - Like or Dislike
 export async function updateRating(prevState, formData) {
   try {
     const getRatingData = formData.get("rating");
     const ratingData = JSON.parse(getRatingData);
+    const { type, modelId, sortedRating, averageRating } = ratingData;
 
-    if (ratingData.serviceRating !== ratingData.averageRating) {
-      let resData = {};
+    let resData = {
+      newRating: 0,
+      newGlobalRating: null,
+      newRatingModelCount: 0,
+    };
 
-      // GET SERVICES COUNT OF SPECIFIC RATING
-      const ratingServices = await getData(
-        RATING_SERVICES_COUNT(ratingData.sortedRating.id)
+    const qvData = {
+      id: modelId,
+      data: {
+        rating: averageRating,
+        rating_global: sortedRating.id,
+      },
+    };
+
+    if (type === "freelancer") {
+      const { updateFreelancer } = await putData(
+        UPDATE_FREELANCER_RATING,
+        qvData
       );
 
-      const ratingServicesCount =
-        ratingServices.data.attributes.services.data.length;
-
-      // POST NEW rating & rating_global
-      const data = {
-        rating: ratingData.averageRating,
-        rating_global: ratingData.sortedRating.id,
-      };
-
-      const res = await putData(
-        SERVICE_RATING_UPDATE(ratingData.serviceId),
-        data
+      const ratingModelCount = await getRatingsModelCount(
+        type,
+        sortedRating.id
       );
 
-      // POST THE RESPONSE DATA
       resData = {
-        newRating: res?.data?.attributes?.rating,
-        newGlobalRating: res?.data?.attributes?.rating_global?.data,
-        newRatingServicesCount: ratingServicesCount,
+        newRating: updateFreelancer.data.attributes.rating,
+        newGlobalRating: updateFreelancer.data.attributes.rating_global?.data,
+        newRatingModelCount: ratingModelCount,
       };
 
       return {
         errors: null,
-        message: "Ratings Updated Successfully!",
+        message: "Freelancer Ratings Updated Successfully!",
         data: resData,
       };
+    } else if (type === "service") {
+    } else if (type === "employer") {
+    } else {
     }
+
+    // if (ratingData.serviceRating !== ratingData.averageRating) {
+    //   let resData = {};
+
+    //   // GET SERVICES COUNT OF SPECIFIC RATING
+    //   const ratingServices = await getData(
+
+    //     RATING_SERVICES_COUNT(ratingData.sortedRating.id)
+    //   );
+
+    //   const ratingServicesCount =
+    //     ratingServices.data.attributes.services.data.length;
+
+    //   // POST NEW rating & rating_global
+    //   const data = {
+    //     rating: ratingData.averageRating,
+    //     rating_global: ratingData.sortedRating.id,
+    //   };
+
+    //   const res = await putData(
+    //     SERVICE_RATING_UPDATE(ratingData.serviceId),
+    //     data
+    //   );
+
+    //   // POST THE RESPONSE DATA
+    //   resData = {
+    //     newRating: res?.data?.attributes?.rating,
+    //     newGlobalRating: res?.data?.attributes?.rating_global?.data,
+    //     newRatingServicesCount: ratingServicesCount,
+    //   };
+
+    //   return {
+    //     errors: null,
+    //     message: "Ratings Updated Successfully!",
+    //     data: resData,
+    //   };
+    // }
   } catch (error) {
     console.error(error);
     return {
