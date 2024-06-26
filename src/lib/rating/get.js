650cec2e564defbfe4734c07d9f52cdcaf5726e0
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-06-26 20:36:57 +0300

    Create get.js

---
+++ src/lib/rating/get.js
@@ -0,0 +1,48 @@
+// "use server";
+
+import { getData } from "../client/operations";
+import {
+  RATINGS,
+  COUNT_FREELANCERS_BY_RATING,
+  COUNT_SERVICES_BY_RATING,
+} from "../graphql/queries";
+
+export async function getRatings() {
+  try {
+    const ratingsData = await getData(RATINGS);
+    const ratings = ratingsData.ratings?.data;
+    return ratings;
+  } catch (error) {
+    console.log("There was an error fetching the ratings!", error?.message);
+    return null;
+  }
+}
+
+export async function getRatingsModelCount(type, id) {
+  try {
+    let count = null;
+
+    if (type === "freelancer") {
+      const { freelancers } = await getData(COUNT_FREELANCERS_BY_RATING, {
+        ratingId: id,
+      });
+
+      count = freelancers?.meta?.pagination?.total || 0;
+    } else if (type === "service") {
+      const { services } = await getData(COUNT_SERVICES_BY_RATING, {
+        ratingId: id,
+      });
+
+      count = services?.meta?.pagination?.total || 0;
+    } else if (type === "employer") {
+      // TODO: Future milestone
+    } else {
+      count = null;
+    }
+
+    return count;
+  } catch (error) {
+    console.log("There was an error fetching the model count!", error?.message);
+    return null;
+  }
+}
