062b420d6b544325800c5ac1f8b33f55610e11d5
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-06-26 20:41:17 +0300

    Create Experience.jsx

---
+++ src/components/ui/profiles/freelancer/Experience.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Experience() {
+  return (
+    <>
+      <h4 className="mb30">Work &amp; Experience</h4>
+      <div className="educational-quality">
+        <div className="m-circle text-thm">M</div>
+        <div className="wrapper mb40">
+          <span className="tag">2012 - 2014</span>
+          <h5 className="mt15">UX Designer</h5>
+          <h6 className="text-thm">Dropbox</h6>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a
+            ipsum tellus. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus.
+          </p>
+        </div>
+        <div className="m-circle before-none text-thm">M</div>
+        <div className="wrapper mb60">
+          <span className="tag">2008 - 2012</span>
+          <h5 className="mt15">Art Director</h5>
+          <h6 className="text-thm">amazon</h6>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a
+            ipsum tellus. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
