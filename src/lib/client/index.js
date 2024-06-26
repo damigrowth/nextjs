0afcfced5aa0c74fc4509eead681682811485916
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-06-26 20:38:19 +0300

    Update index.js

--- src/lib/client/index.js
+++ src/lib/client/index.js
@@ -1,22 +1,64 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support";
-import { STRAPI_GRAPHQL } from "../strapi";
+import { STRAPI_GRAPHQL, STRAPI_TOKEN } from "../strapi";
+
+const customMergeFunction = {
+  UsersPermissionsUserEntity: {
+    merge(existing, incoming, { mergeObjects }) {
+      return mergeObjects(existing, incoming);
+    },
+  },
+  // Define custom merge functions for other types if necessary
+};
+
+const cache = new InMemoryCache();
+
+// const cache = new InMemoryCache({
+//   typePolicies: {
+//     UsersPermissionsUserEntity: {
+//       keyFields: ["id"], // Ensure Apollo knows the unique identifier
+//       fields: {
+//         attributes: customMergeFunction.UsersPermissionsUserEntity,
+//       },
+//     },
+//   },
+// });
 
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
-    cache: new InMemoryCache({
-      typePolicies: {
-        RatingEntity: {
-          keyFields: ["id"], // Specify the primary identifier field
-          merge(existing, incoming, { mergeObjects }) {
-            // Use mergeObjects to deeply merge existing and incoming objects
-            return mergeObjects(existing, incoming);
-          },
-        },
-      },
-    }),
+    // uri: STRAPI_GRAPHQL,
+    // defaultOptions: {
+    //   query: {
+    //     fetchPolicy: "no-cache",
+    //     errorPolicy: "all",
+    //   },
+    // },
+    ssrMode: typeof window === "undefined",
+    cache,
     link: new HttpLink({
       uri: STRAPI_GRAPHQL,
     }),
+    headers: {
+      Authorization: `Bearer ${STRAPI_TOKEN}`,
+    },
   });
 });
+
+// export const { getClient } = registerApolloClient(() => {
+//   return new ApolloClient({
+//     cache: new InMemoryCache({
+//       typePolicies: {
+//         RatingEntity: {
+//           keyFields: ["id"], // Specify the primary identifier field
+//           merge(existing, incoming, { mergeObjects }) {
+//             // Use mergeObjects to deeply merge existing and incoming objects
+//             return mergeObjects(existing, incoming);
+//           },
+//         },
+//       },
+//     }),
+//     link: new HttpLink({
+//       uri: STRAPI_GRAPHQL,
+//     }),
+//   });
+// });
