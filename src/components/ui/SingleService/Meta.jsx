95713e9f0ef351acdf181947715e186737497c82
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-01 16:29:48 +0300

    Update Meta.jsx

--- src/components/ui/SingleService/Meta.jsx
+++ src/components/ui/SingleService/Meta.jsx
@@ -1,4 +1,5 @@
 import UserImage from "@/components/user/UserImage";
+import Image from "next/image";
 import React from "react";
 
 export default function Meta({
@@ -6,11 +7,13 @@ export default function Meta({
   image,
   firstName,
   lastName,
+  username,
   displayName,
   rating,
-  totalReviews,
   views,
   verified,
+  totalReviews,
+  topLevel,
 }) {
   // console.log("meta", firstName, lastName, displayName, image);
   return (
@@ -25,24 +28,48 @@ export default function Meta({
             image={image}
             width={40}
             height={40}
+            path={`/freelancer/${username}`}
           />
+          <div className="tooltip-container">
+            {verified === null || verified === false ? null : (
+              <p className="mb-0 fz14 list-inline-item ml15 ml15-sm mb5-sm ml0-xs">
+                <i className="flaticon-success fa-xl text-thm vam fz24 me-2"></i>
+              </p>
+            )}
+
+            <div className="tooltip" style={{ top: "-50px" }}>
+              Πιστοποιημένος
+            </div>
+          </div>
+
+          <div className="tooltip-container">
+            {topLevel && (
+              <div className="top-badge-inline mb-0 list-inline-item ml5 ml15-sm mb5-sm ml0-xs">
+                <Image
+                  width={22}
+                  height={22}
+                  src="/images/top-badge.png"
+                  alt="top badge"
+                />
+              </div>
+            )}
+
+            <div className="tooltip" style={{ top: "-95px" }}>
+              Έχει λάβει εξαιρετικές αξιολογήσεις
+            </div>
+          </div>
+
           {totalReviews > 0 ? (
-            <p className="mb-0 dark-color fz14 list-inline-item ml25 ml15-sm mb5-sm ml0-xs">
+            <p className="mb-0 fz14 list-inline-item ml15 ml15-sm mb5-sm ml0-xs">
               <i className="fas fa-star vam fz10 review-color mb5"></i>{" "}
-              <span>{rating}</span>
-              <span className="ml5">
+              <span className="dark-color ">{rating}</span>
+              <span className="ml5 review-count-text">
                 {totalReviews === 1
-                  ? `(${totalReviews}) αξιολόγηση`
-                  : `(${totalReviews}) αξιολογήσεις`}
+                  ? `(από ${totalReviews} αξιολόγηση)`
+                  : `(από ${totalReviews} αξιολογήσεις)`}
               </span>
             </p>
           ) : null}
-
-          {verified === null || verified === false ? null : (
-            <p className="mb-0 dark-color fz14 list-inline-item ml25 ml15-sm mb5-sm ml0-xs">
-              <i className="flaticon-badge vam fz20 me-2"></i>Πιστοποιημένος
-            </p>
-          )}
         </div>
       </div>
     </div>
