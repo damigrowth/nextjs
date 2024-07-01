e8453619da0b6fc3d0140a96d7cbf018b35e9a62
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-01 16:30:22 +0300

    Create Terms.jsx

---
+++ src/components/ui/profiles/freelancer/Terms.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { formatDescription } from "@/utils/formatDescription";
+import React from "react";
+
+export default function Terms({ heading, text, border }) {
+  if (!text) {
+    return null;
+  } else {
+    const formattedDescription = formatDescription(text);
+
+    return (
+      <>
+        <h4>{heading}</h4>
+        <div className="freelancer-description text mb30">
+          {formattedDescription}
+        </div>
+        {border && <hr className="opacity-100 mb60 mt60" />}
+      </>
+    );
+  }
+}
