9b299f4ea830a25bdad803ca1c69e9855a68a8a0
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-01 16:29:12 +0300

    Update reaction.js

--- src/lib/review/reaction.js
+++ src/lib/review/reaction.js
@@ -4,20 +4,20 @@ import { revalidatePath } from "next/cache";
 import { putData } from "../api";
 import { REVIEW_REACT } from "../queries";
 
-// React to a Review - Like or Dislike
+// React to a Review - Like or Dislike - REMOVED DISLIKES
 export async function reviewReaction(reaction) {
   try {
     const likesPayload = {
       likes: reaction.likes,
     };
 
-    const dislikesPayload = {
-      dislikes: reaction.dislikes,
-    };
+    // const dislikesPayload = {
+    //   dislikes: reaction.dislikes,
+    // };
 
     await putData(REVIEW_REACT("like", reaction.reviewId), likesPayload);
 
-    await putData(REVIEW_REACT("dislike", reaction.reviewId), dislikesPayload);
+    // await putData(REVIEW_REACT("dislike", reaction.reviewId), dislikesPayload);
   } catch (error) {
     console.error(error);
     return {
