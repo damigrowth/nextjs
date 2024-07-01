3442478da70b38f7ac2cf49c239e15b6001b9a72
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-01 16:29:26 +0300

    Update fragments.js

--- src/lib/graphql/queries/fragments.js
+++ src/lib/graphql/queries/fragments.js
@@ -1,18 +1,42 @@
 import { gql } from "@apollo/client";
 
+const SERVICE = gql`
+  fragment Service on ServiceEntityResponse {
+    data {
+      id
+      attributes {
+        title
+        slug
+      }
+    }
+  }
+`;
+
 const SERVICES = gql`
   fragment Services on ServiceRelationResponseCollection {
     data {
       id
       attributes {
         title
+        slug
       }
     }
   }
 `;
 
-const ORDERS = gql`
-  fragment Orders on OrderRelationResponseCollection {
+const SINGLE_IMAGE = gql`
+  fragment SingleImage on UploadFileEntityResponse {
+    data {
+      id
+      attributes {
+        formats
+      }
+    }
+  }
+`;
+
+const VERIFICATION = gql`
+  fragment Verification on VerificationEntityResponse {
     data {
       id
       attributes {
@@ -29,16 +53,28 @@ const ORDERS = gql`
   }
 `;
 
-const SKILLS = gql`
-  fragment Skills on SkillRelationResponseCollection {
+const USER_PARTIAL = gql`
+  fragment UserPartial on UsersPermissionsUserEntityResponse {
     data {
       id
       attributes {
-        label
-        slug
+        firstName
+        lastName
+        displayName
+        email
+        phone
+        confirmed
+        verification {
+          ...Verification
+        }
+        image {
+          ...SingleImage
+        }
       }
     }
   }
+  ${VERIFICATION}
+  ${SINGLE_IMAGE}
 `;
 
 const BASE = gql`
@@ -66,111 +102,6 @@ const BASE = gql`
         attributes {
           name
         }
-      }
-    }
-  }
-`;
-
-const COVERAGE = gql`
-  fragment Coverage on ComponentLocationCoverage {
-    online
-    counties {
-      data {
-        id
-        attributes {
-          name
-        }
-      }
-    }
-  }
-`;
-
-const FREELANCER_TYPE = gql`
-  fragment FreelancerType on FreelancerTypeEntityResponse {
-    data {
-      id
-      attributes {
-        label
-        slug
-      }
-    }
-  }
-`;
-
-const SIZE = gql`
-  fragment Size on SizeEntityResponse {
-    data {
-      id
-      attributes {
-        label
-        slug
-      }
-    }
-  }
-`;
-
-const VERIFICATION = gql`
-  fragment Verification on VerificationEntityResponse {
-    data {
-      id
-      attributes {
-        status {
-          data {
-            id
-            attributes {
-              type
-            }
-          }
-        }
-      }
-    }
-  }
-`;
-
-const SPECIALISATIONS = gql`
-  fragment Specialisations on SkillRelationResponseCollection {
-    data {
-      id
-      attributes {
-        label
-        slug
-      }
-    }
-  }
-`;
-
-const MIN_BUDGETS = gql`
-  fragment MinBudgets on BudgetRelationResponseCollection {
-    data {
-      id
-      attributes {
-        value
-        label
-        slug
-      }
-    }
-  }
-`;
-
-const INDUSTRIES = gql`
-  fragment Industries on IndustryRelationResponseCollection {
-    data {
-      id
-      attributes {
-        label
-        slug
-      }
-    }
-  }
-`;
-
-const CONTACT_TYPES = gql`
-  fragment ContactTypes on ContactTypeRelationResponseCollection {
-    data {
-      id
-      attributes {
-        label
-        slug
       }
     }
   }
@@ -213,6 +144,160 @@ const SOCIALS = gql`
   }
 `;
 
+const PAYMENT_METHOD = gql`
+  fragment PaymentMethod on PaymentMethodRelationResponseCollection {
+    data {
+      id
+      attributes {
+        label
+        slug
+      }
+    }
+  }
+`;
+
+const SETTLEMENT_METHOD = gql`
+  fragment SettlementMethod on SettlementMethodRelationResponseCollection {
+    data {
+      id
+      attributes {
+        label
+        slug
+      }
+    }
+  }
+`;
+
+const CONTACT_TYPES = gql`
+  fragment ContactTypes on ContactTypeRelationResponseCollection {
+    data {
+      id
+      attributes {
+        label
+        slug
+      }
+    }
+  }
+`;
+
+const FREELANCER_TYPE = gql`
+  fragment FreelancerType on FreelancerTypeEntityResponse {
+    data {
+      id
+      attributes {
+        label
+        slug
+      }
+    }
+  }
+`;
+
+const FREELANCER_CATEGORY = gql`
+  fragment FreelancerCategory on FreelancerCategoryEntityResponse {
+    data {
+      id
+      attributes {
+        label
+        plural
+        slug
+      }
+    }
+  }
+`;
+
+const ORDERS = gql`
+  fragment Orders on OrderRelationResponseCollection {
+    data {
+      id
+      attributes {
+        status {
+          data {
+            id
+            attributes {
+              type
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const SKILLS = gql`
+  fragment Skills on SkillRelationResponseCollection {
+    data {
+      id
+      attributes {
+        label
+        slug
+      }
+    }
+  }
+`;
+
+const COVERAGE = gql`
+  fragment Coverage on ComponentLocationCoverage {
+    online
+    counties {
+      data {
+        id
+        attributes {
+          name
+        }
+      }
+    }
+  }
+`;
+
+const SIZE = gql`
+  fragment Size on SizeEntityResponse {
+    data {
+      id
+      attributes {
+        label
+        slug
+      }
+    }
+  }
+`;
+
+const SPECIALISATIONS = gql`
+  fragment Specialisations on SkillRelationResponseCollection {
+    data {
+      id
+      attributes {
+        label
+        slug
+      }
+    }
+  }
+`;
+
+const MIN_BUDGETS = gql`
+  fragment MinBudgets on BudgetRelationResponseCollection {
+    data {
+      id
+      attributes {
+        value
+        label
+        slug
+      }
+    }
+  }
+`;
+
+const INDUSTRIES = gql`
+  fragment Industries on IndustryRelationResponseCollection {
+    data {
+      id
+      attributes {
+        label
+        slug
+      }
+    }
+  }
+`;
+
 const TAG = gql`
   fragment Tag on TagEntity {
     id
@@ -247,17 +332,6 @@ const RATING = gql`
   }
 `;
 
-const SINGLE_IMAGE = gql`
-  fragment SingleImage on UploadFileEntityResponse {
-    data {
-      id
-      attributes {
-        formats
-      }
-    }
-  }
-`;
-
 const MULTIPLE_IMAGES = gql`
   fragment MultipleImages on UploadFileRelationResponseCollection {
     data {
@@ -269,30 +343,6 @@ const MULTIPLE_IMAGES = gql`
   }
 `;
 
-const USER_PARTIAL = gql`
-  fragment UserPartial on UsersPermissionsUserEntityResponse {
-    data {
-      id
-      attributes {
-        firstName
-        lastName
-        displayName
-        email
-        phone
-        confirmed
-        verification {
-          ...Verification
-        }
-        image {
-          ...SingleImage
-        }
-      }
-    }
-  }
-  ${VERIFICATION}
-  ${SINGLE_IMAGE}
-`;
-
 const AREA = gql`
   fragment Area on AreaEntityResponse {
     data {
@@ -302,28 +352,6 @@ const AREA = gql`
       }
     }
   }
-`;
-
-const FREELANCER = gql`
-  fragment Freelancer on FreelancerEntityResponse {
-    data {
-      id
-      attributes {
-        tagline
-        rating
-        rate
-        username
-        base {
-          ...Base
-        }
-        user {
-          ...UserPartial
-        }
-      }
-    }
-  }
-  ${USER_PARTIAL}
-  ${BASE}
 `;
 
 const ADDONS = gql`
@@ -475,30 +503,6 @@ const ROLE = gql`
   }
 `;
 
-const PAYMENT_METHOD = gql`
-  fragment PaymentMethod on PaymentMethodRelationResponseCollection {
-    data {
-      id
-      attributes {
-        label
-        slug
-      }
-    }
-  }
-`;
-
-const SETTLEMENT_METHOD = gql`
-  fragment SettlementMethod on SettlementMethodRelationResponseCollection {
-    data {
-      id
-      attributes {
-        label
-        slug
-      }
-    }
-  }
-`;
-
 const PAGINATION = gql`
   fragment Pagination on ResponseCollectionMeta {
     pagination {
@@ -511,13 +515,14 @@ const PAGINATION = gql`
 `;
 
 export {
+  SERVICE,
   SERVICES,
   ORDERS,
   SKILLS,
   BASE,
   COVERAGE,
-  FREELANCER,
   FREELANCER_TYPE,
+  FREELANCER_CATEGORY,
   SIZE,
   VERIFICATION,
   SPECIALISATIONS,
