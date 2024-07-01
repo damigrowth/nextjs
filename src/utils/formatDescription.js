0454d1b811a97e70079b14042d422a9e7bc214a1
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-01 16:29:05 +0300

    Create formatDescription.js

---
+++ src/utils/formatDescription.js
@@ -0,0 +1,13 @@
+export const formatDescription = (text) => {
+  const desc = text
+    .split("\n")
+    .map((text, index) =>
+      text.trim() !== "" ? (
+        <p key={index}>{text}</p>
+      ) : (
+        <div key={index} className="line-break"></div>
+      )
+    );
+
+  return desc;
+};
