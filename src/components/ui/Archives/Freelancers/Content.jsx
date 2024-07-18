78ff142fd248d80685c659517114d074c5f7b714
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-18 23:42:06 +0300

    Create Content.jsx

---
+++ src/components/ui/Archives/Freelancers/Content.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Topbar from "../Topbar";
+import FreelancerGrid from "./FreelancerGrid";
+import Pagination from "../Pagination";
+import { freelancerSortOptions } from "../options";
+import { FREELANCERS_ARCHIVE } from "@/lib/graphql/queries";
+import { getData } from "@/lib/client/operations";
+
+export default async function Content({ paramsFilters }) {
+  const { freelancers } = await getData(FREELANCERS_ARCHIVE, paramsFilters);
+
+  return (
+    <div className="col-lg-9">
+      <Topbar
+        meta={freelancers?.meta?.pagination}
+        single="επαγγελματίας"
+        plural="επαγγελματίες"
+        sortOptions={freelancerSortOptions}
+      />
+
+      <FreelancerGrid freelancers={freelancers?.data} />
+
+      <div className="row mt30">
+        <Pagination
+          meta={freelancers?.meta?.pagination}
+          plural="επαγγελματίες"
+        />
+      </div>
+    </div>
+  );
+}
