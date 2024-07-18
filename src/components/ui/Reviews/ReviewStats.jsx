65d1a42ef28facb568712913a1f61b8dda626986
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-18 23:38:42 +0300

    Create ReviewStats.jsx

---
+++ src/components/ui/Reviews/ReviewStats.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import ReviewStatsForm from "../forms/ReviewStatsForm";
+import ReviewStatsLine from "./ReviewStatsLine";
+
+export default function ReviewStats({
+  rating,
+  reviews_total,
+  rating_global,
+  ratingStars,
+}) {
+  const stars = [
+    {
+      stars: 5,
+      count: ratingStars[4],
+    },
+    {
+      stars: 4,
+      count: ratingStars[3],
+    },
+    {
+      stars: 3,
+      count: ratingStars[2],
+    },
+    {
+      stars: 2,
+      count: ratingStars[1],
+    },
+    {
+      stars: 1,
+      count: ratingStars[0],
+    },
+  ];
+
+  // Determine the maximum count for normalization
+  const maxCount = Math.max(...stars.map((star) => star.count));
+
+  // Calculate the value for each star rating based on the maximum count
+  const reviewStatsData = stars.map((star) => ({
+    stars: star.stars,
+    count: star.count,
+    value: Number(
+      maxCount === 0 ? 0 : ((star.count / maxCount) * 100).toFixed(2)
+    ),
+  }));
+
+  return (
+    <div className="col-lg-12">
+      <div className="total_review mb30 mt45">
+        <h4>{reviews_total} Αξιολογήσεις</h4>
+      </div>
+      <div className="d-md-flex align-items-center mb30">
+        <div className="total-review-box d-flex align-items-center text-center mb30-sm">
+          <div className="wrapper mx-auto">
+            <div className="t-review mb15">{rating}</div>
+            <h5>
+              {rating_global?.attributes && rating_global.attributes.label}
+            </h5>
+            {/* <p className="fz14 mb-0">
+        {handleLabelModelCount(ratingModelCount || newRatingModelCount)}
+      </p> */}
+          </div>
+        </div>
+        <div className="wrapper ml60 ml0-sm">
+          {reviewStatsData.map((stat, i) => (
+            <div key={i}>
+              <ReviewStatsLine data={stat} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
