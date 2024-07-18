0d94efea0a059cf0c09277135ace50c60252e9f5
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-18 23:37:52 +0300

    Update index.js

--- src/lib/graphql/queries/index.js
+++ src/lib/graphql/queries/index.js
@@ -4,15 +4,26 @@ import {
   FEATURED_SERVICE_RELATIONS,
   FREELANCER_MAIN,
   FREELANCER_RELATIONS,
+  FREELANCER_SEO,
   REVIEW,
   REVIEW_MAIN,
   REVIEW_RELATIONS,
   SERVICE_MAIN,
+  SERVICE_PARTIAL,
+  SERVICE_PARTIAL_MAIN,
+  SERVICE_PARTIAL_RELATIONS,
   SERVICE_RELATIONS,
+  SERVICE_SEO,
   USER_MAIN,
   USER_RELATIONS,
 } from "./parts";
-import { CATEGORY, PAGINATION, SINGLE_IMAGE, TAG } from "./fragments";
+import {
+  CATEGORY,
+  FREELANCER_REFERENCE,
+  PAGINATION,
+  SINGLE_IMAGE,
+  TAG,
+} from "./fragments";
 
 const SERVICE_BY_SLUG = gql`
   query GetService($slug: String!) {
@@ -30,6 +41,19 @@ const SERVICE_BY_SLUG = gql`
   ${SERVICE_RELATIONS}
 `;
 
+const SERVICE_SEO_BY_SLUG = gql`
+  query GetServiceSEO($slug: String!) {
+    services(filters: { slug: { eq: $slug } }) {
+      data {
+        attributes {
+          ...ServiceSEO
+        }
+      }
+    }
+  }
+  ${SERVICE_SEO}
+`;
+
 const FREELANCER_BY_USERNAME = gql`
   query GetFreelancer($username: String!) {
     freelancers(filters: { username: { eq: $username } }) {
@@ -44,6 +68,19 @@ const FREELANCER_BY_USERNAME = gql`
   }
   ${FREELANCER_MAIN}
   ${FREELANCER_RELATIONS}
+`;
+
+const FREELANCER_SEO_BY_USERNAME = gql`
+  query GetFreelancerSEO($username: String!) {
+    freelancers(filters: { username: { eq: $username } }) {
+      data {
+        attributes {
+          ...FreelancerSEO
+        }
+      }
+    }
+  }
+  ${FREELANCER_SEO}
 `;
 
 const RATINGS = gql`
@@ -261,12 +298,211 @@ const ALL_REVIEWS_RATINGS_BY_SERVICE = gql`
   }
 `;
 
+const SERVICE_UID = gql`
+  query GetServiceUid($id: ID!) {
+    service(id: $id) {
+      data {
+        id
+        attributes {
+          title
+        }
+      }
+    }
+  }
+`;
+
+const SERVICES_ARCHIVE = gql`
+  query ServicesArchive(
+    $min: Int
+    $max: Int
+    $time: Int
+    $cat: ID
+    $verified: Boolean
+    $page: Int
+    $sort: [String]
+  ) {
+    services(
+      filters: {
+        price: { gte: $min, lte: $max }
+        time: { lte: $time }
+        category: { id: { eq: $cat } }
+        freelancer: { user: { verified: { eq: $verified } } }
+      }
+      sort: $sort
+      pagination: { page: $page, pageSize: 5 }
+    ) {
+      data {
+        id
+        attributes {
+          ...ServicePartialMain
+          ...ServicePartialRelations
+        }
+      }
+      meta {
+        ...Pagination
+      }
+    }
+  }
+  ${SERVICE_PARTIAL_MAIN}
+  ${SERVICE_PARTIAL_RELATIONS}
+  ${PAGINATION}
+`;
+
+const CATEGORIES_SEARCH = gql`
+  query CategoriesSearch($label: String) {
+    categories(filters: { label: { contains: $label } }, sort: "label:desc") {
+      data {
+        ...Category
+      }
+    }
+  }
+  ${CATEGORY}
+`;
+
+const FREELANCERS_ARCHIVE = gql`
+  query FreelancersArchive(
+    $min: Int
+    $max: Int
+    $paymentMethods: [ID]
+    $contactTypes: [ID]
+    $coverageOnline: Boolean
+    $coverageCounties: [ID]
+    $type: ID
+    $cat: ID
+    $specializations: [ID]
+    $experience: Int
+    $top: Boolean
+    $sort: [String]
+    $page: Int
+  ) {
+    freelancers(
+      filters: {
+        rate: { gte: $min, lte: $max }
+        payment_methods: { id: { in: $paymentMethods } }
+        contactTypes: { id: { in: $contactTypes } }
+        coverage: {
+          online: { eq: $coverageOnline }
+          counties: { id: { in: $coverageCounties } }
+        }
+        type: { id: { eq: $type } }
+        category: { id: { eq: $cat } }
+        specialisations: { id: { in: $specializations } }
+        yearsOfExperience: { lte: $experience }
+        topLevel: { eq: $top }
+      }
+      sort: $sort
+      pagination: { page: $page, pageSize: 9 }
+    ) {
+      data {
+        id
+        attributes {
+          ...FreelancerReference
+        }
+      }
+      meta {
+        ...Pagination
+      }
+    }
+  }
+  ${PAGINATION}
+  ${FREELANCER_REFERENCE}
+`;
+
+const PAYMENT_METHODS = gql`
+  query PaymentMethods {
+    paymentMethods {
+      data {
+        id
+        attributes {
+          label
+          slug
+        }
+      }
+    }
+  }
+`;
+
+const CONTACT_TYPES = gql`
+  query ContactTypes {
+    contactTypes {
+      data {
+        id
+        attributes {
+          label
+          slug
+        }
+      }
+    }
+  }
+`;
+
+const FREELANCER_TYPES = gql`
+  query FreelancerTypes {
+    freelancerTypes {
+      data {
+        id
+        attributes {
+          label
+          slug
+        }
+      }
+    }
+  }
+`;
+
+const FREELANCER_CATEGORIES_SEARCH = gql`
+  query FreelancerCategoriesSearch($label: String) {
+    freelancerCategories(
+      filters: { label: { contains: $label } }
+      sort: "label:desc"
+    ) {
+      data {
+        id
+        attributes {
+          label
+          plural
+          slug
+        }
+      }
+    }
+  }
+`;
+
+const SPECIALIZATIONS = gql`
+  query Specializations {
+    skills {
+      data {
+        id
+        attributes {
+          label
+          slug
+        }
+      }
+    }
+  }
+`;
+
+const COUNTIES_SEARCH = gql`
+  query CountiesSearch($name: String) {
+    counties(filters: { name: { contains: $name } }, sort: "name:asc") {
+      data {
+        id
+        attributes {
+          name
+        }
+      }
+    }
+  }
+`;
+
 export {
   GET_ME,
   USER_BY_ID,
   USER_BY_ID_BASIC,
   SERVICE_BY_SLUG,
+  SERVICE_SEO_BY_SLUG,
   FREELANCER_BY_USERNAME,
+  FREELANCER_SEO_BY_USERNAME,
   RATINGS,
   CATEGORIES,
   TAGS,
@@ -277,4 +513,14 @@ export {
   ALL_REVIEWS_RATINGS_BY_FREELANCER,
   REVIEWS_BY_SERVICE,
   ALL_REVIEWS_RATINGS_BY_SERVICE,
+  SERVICE_UID,
+  SERVICES_ARCHIVE,
+  CATEGORIES_SEARCH,
+  FREELANCERS_ARCHIVE,
+  PAYMENT_METHODS,
+  CONTACT_TYPES,
+  FREELANCER_TYPES,
+  FREELANCER_CATEGORIES_SEARCH,
+  SPECIALIZATIONS,
+  COUNTIES_SEARCH,
 };
