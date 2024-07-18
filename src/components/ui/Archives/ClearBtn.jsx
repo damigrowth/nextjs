3acb7316c083c995804bc66ad601c4a0ddd09f19
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-18 23:42:09 +0300

    Create ClearBtn.jsx

---
+++ src/components/ui/Archives/ClearBtn.jsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React, { useCallback, useTransition } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+export default function ClearBtn() {
+  const router = useRouter();
+  const pathname = usePathname(); // Use the current pathname
+
+  const [isPending, startTransition] = useTransition();
+
+  const clearHandler = () => {
+    startTransition(() => {
+      router.push(pathname, {
+        scroll: false,
+      });
+    });
+  };
+
+  return (
+    <a
+      href="#archive"
+      disabled={isPending}
+      onClick={clearHandler}
+      className="ud-btn btn-thm ui-clear-btn w-100"
+    >
+      Καθαρισμός
+      {isPending ? (
+        <div className="spinner-border spinner-border-sm ml10" role="status">
+          <span className="sr-only"></span>
+        </div>
+      ) : (
+        <span className="fas fa-trash-can pl10 "></span>
+      )}
+    </a>
+  );
+}
