72b7202f93728b0b2f99df94f1ac3a0d3052e848
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-18 23:41:54 +0300

    Create Coverage.jsx

---
+++ src/components/ui/Archives/Freelancers/Filters/Coverage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Switch from "../../Inputs/Switch";
+import SearchSelectSingle from "../../Inputs/SearchSelectSingle";
+
+export default function Coverage({ counties }) {
+  const options = counties.map((el) => ({
+    value: el.id,
+    label: el.attributes.name,
+  }));
+
+  return (
+    <>
+      <Switch paramName="cov_o" label={"Online"} />
+      <SearchSelectSingle
+        defaultLabel="Όλες οι περιοχές"
+        paramOptionName="cov_c"
+        paramSearchName="cov_c_s"
+        options={options}
+      />
+    </>
+  );
+}
