844d774608804be092f88b27ba153a950a7a92b7
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-18 23:40:14 +0300

    Create Topbar.jsx

---
+++ src/components/ui/Archives/Topbar.jsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import React from "react";
+import SortOptions from "./SortOptions";
+
+export default function Topbar({ meta, single, plural, sortOptions }) {
+  let total = 0;
+
+  if (meta.total === 0) {
+    total = `0 ${single}`;
+  } else if (meta.total === 1) {
+    total = meta.total + " " + single;
+  } else if (meta.total > 1) {
+    total = meta.total + " " + plural;
+  }
+
+  return (
+    <div className="row align-items-center mb20">
+      <div className="col-md-6">
+        <div className="text-center text-md-start">
+          <p className="text mb-0 mb10-sm">
+            <span className="fw500">{total}</span>
+          </p>
+        </div>
+      </div>
+      <div className="col-md-6">
+        <div className="page_control_shorting d-md-flex align-items-center justify-content-center justify-content-md-end">
+          <div className="dropdown-lists d-block d-lg-none me-2 mb10-sm">
+            <ul className="p-0 mb-0 text-center text-md-start">
+              <li>
+                <button
+                  // onClick={listingToggle}
+                  type="button"
+                  className="open-btn filter-btn-left"
+                >
+                  <Image
+                    height={18}
+                    width={18}
+                    className="me-2"
+                    src="/images/icon/all-filter-icon.svg"
+                    alt="icon"
+                  />
+                  Όλα τα φίλτρα
+                </button>
+              </li>
+            </ul>
+          </div>
+          <SortOptions sortOptions={sortOptions} />
+        </div>
+      </div>
+    </div>
+  );
+}
