cf13c2ab2218ce464d78c5c4e028de139e99f36a
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-18 23:41:35 +0300

    Create Type.jsx

---
+++ src/components/ui/Archives/Freelancers/Filters/Type.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import RadioBox from "../../Inputs/RadioBox";
+import { getData } from "@/lib/client/operations";
+import { FREELANCER_TYPES } from "@/lib/graphql/queries";
+
+export default async function Type() {
+  const { freelancerTypes } = await getData(FREELANCER_TYPES);
+
+  const options = freelancerTypes.data.map((el) => ({
+    value: el.id,
+    label: el.attributes.label,
+  }));
+
+  return <RadioBox options={options} paramName="type" />;
+}
