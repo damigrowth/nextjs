cd4ed4a1e5e043d60ddb1a5f16df90f482b520e8
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-18 23:39:02 +0300

    Update Reviews.jsx

--- src/components/ui/Reviews/Reviews.jsx
+++ src/components/ui/Reviews/Reviews.jsx
@@ -1,88 +1,31 @@
-import Link from "next/link";
-import ReviewStatsForm from "../forms/ReviewStatsForm";
-import ReviewStatsLine from "./ReviewStatsLine";
 import Review from "./Review";
-import {
-  COUNT_FREELANCERS_BY_RATING,
-  COUNT_SERVICES_BY_RATING,
-} from "@/lib/graphql/queries";
-// import { getRatingsModelCount } from "@/lib/rating/get";  // -- Removed --
 import LoadMoreBtn from "../profiles/freelancer/LoadMoreBtn";
+import ReviewStats from "./ReviewStats";
 
 export default async function Reviews({
-  type,
-  modelId,
   reviews,
-  ratings,
   rating,
+  reviews_total,
   rating_global,
-  reviewsMeta,
   reviewsPage,
-  allReviewsRatings,
   showReviewsModel,
+  ratingStars,
 }) {
   if (reviews.length === 0) {
     return <h4 className="mt40 mb20">Χωρίς Αξιολογήσεις</h4>;
   }
-  // This outputs: [ 4, 3 ]
-  const reviewRatings = allReviewsRatings.map(
-    (review) => review?.attributes?.rating
-  );
-
-  const ratingIds = ratings.map(({ id }) => Number(id));
-
-  // Calculate the count for each star rating
-  const starCounts = ratingIds.map((star) => ({
-    stars: star,
-    count: reviewRatings.filter((rating) => rating === star).length,
-  }));
-
-  // Determine the maximum count for normalization
-  const maxCount = Math.max(...starCounts.map((star) => star.count));
-
-  // Calculate the value for each star rating based on the maximum count
-  const reviewStats = starCounts.map((star) => ({
-    stars: Number(star.stars),
-    count: Number(star.count),
-    value: Number(
-      maxCount === 0 ? 0 : ((star.count / maxCount) * 100).toFixed(2)
-    ),
-  }));
-
-  // -- Removed --
-  // const ratingModelCount = await getRatingsModelCount(type, rating_global.id);
 
   return (
     <div className="px30 bdr1 pt30 pb-0 mb30 bg-white bdrs12 wow fadeInUp default-box-shadow1">
       <div className="product_single_content mb50">
         <div className="mbp_pagination_comments">
           <div className="row">
-            <div className="col-lg-12">
-              <div className="total_review mb30 mt45">
-                <h4>{reviewsMeta.total} Αξιολογήσεις</h4>
-              </div>
-              <div className="d-md-flex align-items-center mb30">
-                <div className="total-review-box d-flex align-items-center text-center mb30-sm">
-                  <ReviewStatsForm
-                    type={type}
-                    modelId={modelId}
-                    ratings={ratings}
-                    reviewRatings={reviewRatings}
-                    rating={rating}
-                    rating_global={rating_global}
-                    allReviewsRatings={allReviewsRatings}
-                    // ratingModelCount={ratingModelCount}  // -- Removed --
-                  />
-                </div>
-                <div className="wrapper ml60 ml0-sm">
-                  {reviewStats.map((stat, i) => (
-                    <div key={i}>
-                      <ReviewStatsLine data={stat} />
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
+            <ReviewStats
+              rating={rating}
+              reviews_total={reviews_total}
+              rating_global={rating_global}
+              ratingStars={ratingStars}
+            />
             <ul>
               {reviews.map(({ attributes: review, id }, i) => (
                 <li key={i}>
@@ -109,7 +52,7 @@ export default async function Reviews({
             <div className="col-md-12">
               <LoadMoreBtn
                 name="Αξιολογήσεις"
-                total={reviewsMeta.total}
+                total={reviews_total}
                 count={reviews.length}
                 paramsName="reviews"
                 paramsPage={reviewsPage}
