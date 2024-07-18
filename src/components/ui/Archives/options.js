f127b37515c9a39ff89bee48bacabe8af6729928
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-18 23:41:02 +0300

    Create options.js

---
+++ src/components/ui/Archives/options.js
@@ -0,0 +1,67 @@
+export const serviceSortOptions = [
+  {
+    value: "publishedAt:desc",
+    label: "Πιο πρόσφατες",
+  },
+  {
+    value: "publishedAt:asc",
+    label: "Πιο παλιές",
+  },
+  {
+    value: "price:asc",
+    label: "Αύξουσα τιμή",
+  },
+  {
+    value: "price:desc",
+    label: "Φθίνουσα τιμή",
+  },
+];
+
+export const freelancerSortOptions = [
+  {
+    value: "publishedAt:desc",
+    label: "Πιο πρόσφατα",
+  },
+  {
+    value: "publishedAt:asc",
+    label: "Πιο παλιά",
+  },
+  {
+    value: "rate:asc",
+    label: "Αύξουσα εργατοώρα",
+  },
+  {
+    value: "rate:desc",
+    label: "Φθίνουσα εργατοώρα",
+  },
+];
+
+export const serviceTimeOptions = [
+  {
+    value: 3,
+    label: "Μέχρι 3 ημέρες",
+  },
+  {
+    value: 7,
+    label: "Μέχρι 7 ημέρες",
+  },
+  {
+    value: 30,
+    label: "Μέχρι 30 ημέρες",
+  },
+];
+
+export const freelancerExperienceOptions = [
+  {
+    value: 1,
+    label: "1+",
+  },
+  {
+    value: 3,
+    label: "3+",
+  },
+  {
+    value: 5,
+    label: "5+",
+  },
+];
