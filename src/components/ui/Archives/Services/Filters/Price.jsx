63e7d9a57846e30973ac5517f6436c76e9f0bf18
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-18 23:40:44 +0300

    Create Price.jsx

---
+++ src/components/ui/Archives/Services/Filters/Price.jsx
@@ -0,0 +1,7 @@
+import PriceDropdown1 from "@/components/dropdown/PriceDropdown1";
+import React from "react";
+import Slider from "../../Inputs/RangeSlider";
+
+export default function Price() {
+  return <Slider iniMin={5} iniMax={5000} />;
+}
