53bb65ba373202918b8ae9bd19cadaf42dc01f66
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-19 20:49:06 +0300

    Create ServiceGrid.jsx

---
+++ src/components/ui/Archives/Services/ServiceGrid.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ServiceCard from "./ServiceCard";
+
+export default function ServiceGrid({ services }) {
+  return (
+    <div className="row">
+      <div className="col-lg-12">
+        {services.length > 0 ? (
+          services.map((service, i) => (
+            <div key={i}>
+              <ServiceCard service={service} />
+            </div>
+          ))
+        ) : (
+          <div>Δεν βρέθηκαν υπηρεσίες</div>
+        )}
+      </div>
+    </div>
+  );
+}
