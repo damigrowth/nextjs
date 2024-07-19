cda68a5a18036daa0a43cb26ff9d19813643eb7a
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-19 20:49:16 +0300

    Update Content.jsx

--- src/components/ui/Archives/Services/Content.jsx
+++ src/components/ui/Archives/Services/Content.jsx
@@ -1,35 +1,25 @@
 import React from "react";
 import Topbar from "../Topbar";
 import { serviceSortOptions } from "../options";
-import ServiceCard from "./ServiceCard";
 import Pagination from "../Pagination";
 import { SERVICES_ARCHIVE } from "@/lib/graphql/queries";
 import { getData } from "@/lib/client/operations";
+import ServiceGrid from "./ServiceGrid";
 
 export default async function Content({ paramsFilters }) {
   const { services } = await getData(SERVICES_ARCHIVE, paramsFilters);
   return (
-    <div className="col-lg-9">
+    <>
       <Topbar
         meta={services?.meta?.pagination}
         single="υπηρεσία"
         plural="υπηρεσίες"
         sortOptions={serviceSortOptions}
       />
-      <div className="row">
-        <div className="col-lg-12">
-          {services.data.length > 0 ? (
-            services.data.map((service, i) => (
-              <div key={i}>
-                <ServiceCard service={service} />
-              </div>
-            ))
-          ) : (
-            <div>Δεν βρέθηκαν υπηρεσίες</div>
-          )}
-        </div>
+      <ServiceGrid services={services?.data} />
+      <div className="row mt30">
+        <Pagination meta={services?.meta?.pagination} plural="υπηρεσίες" />
       </div>
-      <Pagination meta={services?.meta?.pagination} plural="υπηρεσίες" />
-    </div>
+    </>
   );
 }
