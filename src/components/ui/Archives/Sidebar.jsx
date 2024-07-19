f34aa7a86cd969c3a313bac9db8e2b989b36fb2e
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-19 20:48:58 +0300

    Update Sidebar.jsx

--- src/components/ui/Archives/Sidebar.jsx
+++ src/components/ui/Archives/Sidebar.jsx
@@ -7,34 +7,42 @@ export default function Sidebar({ filters }) {
   return (
     <div className="list-sidebar-style1 d-none d-lg-block">
       <div className="accordion" id="accordionExample">
-        {filters.map((filter, index) => (
-          <div className="card mb20 pb10 mt-0" key={index}>
-            <div className="card-header" id={`heading${index}`}>
-              <h4>
-                <button
-                  className={`btn btn-link ps-0 pt-0 ${
-                    index === 0 ? "" : "collapsed"
-                  }`}
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target={`#collapse${index}`}
-                  aria-expanded={index === 0 ? "true" : "false"}
-                  aria-controls={`collapse${index}`}
-                >
-                  {filter.heading}
-                </button>
-              </h4>
+        {filters.map((filter, index) => {
+          const paramsArray = Array.isArray(filter.params)
+            ? filter.params
+            : [filter.params];
+          const hasParams = paramsArray.some(
+            (param) => param !== undefined && param !== null && param !== ""
+          );
+          const isCollapsed = index !== 0 && !hasParams;
+
+          return (
+            <div className="card mb20 pb10 mt-0" key={index}>
+              <button
+                className={`btn btn-link ps-0 pt-0 ${
+                  isCollapsed ? "collapsed" : ""
+                }`}
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target={`#collapse${index}`}
+                aria-expanded={!isCollapsed ? "true" : "false"}
+                aria-controls={`collapse${index}`}
+              >
+                <div className="card-header" id={`heading${index}`}>
+                  <h4 style={{ textAlign: "left" }}>{filter.heading}</h4>
+                </div>
+              </button>
+              <div
+                id={`collapse${index}`}
+                className={`collapse ${!isCollapsed ? "show" : ""}`}
+                aria-labelledby={`heading${index}`}
+                data-parent="#accordionExample"
+              >
+                {filter.component}
+              </div>
             </div>
-            <div
-              id={`collapse${index}`}
-              className={`collapse ${index === 0 ? "show" : ""}`}
-              aria-labelledby={`heading${index}`}
-              data-parent="#accordionExample"
-            >
-              {filter.component}
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
       <ClearBtn />
     </div>
