82377d91274ed5fd1c5f1d6196e6c3f113c31e84
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-19 20:49:02 +0300

    Update ServicesArchive.jsx

--- src/components/ui/Archives/Services/ServicesArchive.jsx
+++ src/components/ui/Archives/Services/ServicesArchive.jsx
@@ -11,33 +11,48 @@ import { serviceSortOptions } from "../options";
 import Pagination from "../Pagination";
 import Content from "./Content";
 import ContentSkeleton from "./ContentSkeleton";
+import BorderSpinner from "../../Spinners/BorderSpinner";
 
 export default function ServicesArchive({
   categories,
   searchParams,
   paramsFilters,
 }) {
+  const { min, max, time, cat, ver } = searchParams;
+
+  // Remove 'cat_s' from searchParams
+  const filteredSearchParams = Object.fromEntries(
+    Object.entries(searchParams).filter(([key]) => key !== "cat_s")
+  );
+
   const filters = [
-    { heading: "Τιμή", component: <Price /> },
-    { heading: "Χρόνος παράδοσης", component: <Time /> },
-    { heading: "Κατηγορία", component: <Category categories={categories} /> },
-    { heading: "Πιστοποιημένο προφίλ", component: <Verified /> },
+    { heading: "Τιμή", params: [min, max], component: <Price /> },
+    { heading: "Χρόνος παράδοσης", params: time, component: <Time /> },
+    {
+      heading: "Κατηγορία",
+      params: cat,
+      component: <Category categories={categories} />,
+    },
+    { heading: "Πιστοποιημένο προφίλ", params: ver, component: <Verified /> },
   ];
 
   return (
     <>
-      <section className="pt30 pb90">
+      <section id="archive" className="pt30 pb90">
         <div className="container">
-          <div className="row">
+          <div className="row data-loading-section">
             <div className="col-lg-3">
               <Sidebar filters={filters} />
             </div>
-            <Suspense
-              key={JSON.stringify(searchParams)}
-              fallback={<ContentSkeleton />}
-            >
-              <Content paramsFilters={paramsFilters} />
-            </Suspense>
+            <div className="col-lg-9 archive-content">
+              <BorderSpinner className="archive-content-spinner" />
+              <Suspense
+                key={JSON.stringify(filteredSearchParams)}
+                fallback={<ContentSkeleton />}
+              >
+                <Content paramsFilters={paramsFilters} />
+              </Suspense>
+            </div>
           </div>
         </div>
       </section>
