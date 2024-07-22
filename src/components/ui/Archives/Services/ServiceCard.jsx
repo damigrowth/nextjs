1f4955f09734525b0b6da8484db90cc089860a25
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-22 16:29:18 +0300

    Update ServiceCard.jsx

--- src/components/ui/Archives/Services/ServiceCard.jsx
+++ src/components/ui/Archives/Services/ServiceCard.jsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import ServiceCardMedia from "./ServiceCardMedia";
+import VerifiedBadge from "@/components/user/VerifiedBadge";
+import TopLevelBadge from "@/components/user/TopLevelBadge";
 
 export default function ServiceCard({ service }) {
   const {
@@ -24,43 +26,68 @@ export default function ServiceCard({ service }) {
 
   return (
     <div className="data-loading-element listing-style1 list-style d-block d-xl-flex align-items-center">
-      <ServiceCardMedia media={media?.data} />
+      {slug ? (
+        <Link href={`/s/${slug}`}>
+          <ServiceCardMedia media={media?.data} />
+        </Link>
+      ) : (
+        <ServiceCardMedia media={media?.data} />
+      )}
+
       <div className="list-content flex-grow-1 ms-1">
         <a className="listing-fav fz12">
           <span className="far fa-heart" />
         </a>
-        <p className="list-text body-color fz14 mb-1">
-          {category?.data?.attributes?.label}
-        </p>
-        <h5 className="list-title">
-          <Link href={`/s/${slug}`}>{title}</Link>
-        </h5>
+        <div className="archive-service-card-meta">
+          <div>
+            <h5 className="list-title">
+              <Link href={`/s/${slug}`}>{title}</Link>
+            </h5>
+            <p className="list-text body-color fz14 mb-1">
+              {category?.data?.attributes?.label}
+            </p>
+          </div>
 
-        <div className="review-meta d-flex align-items-center">
-          {reviews_total && reviews_total > 0 && (
-            <>
-              <i className="fas fa-star fz10 review-color me-2" />
-              <p className="mb-0 body-color fz14">
-                <span className="dark-color me-2">{rating}</span>
-                {reviews_total > 1
-                  ? reviews_total + " " + "αξιολογήσεις"
-                  : reviews_total + " " + "αξιολόγηση"}
-              </p>
-            </>
-          )}
+          <div className="review-meta d-flex align-items-center">
+            {reviews_total && reviews_total > 0 && (
+              <>
+                <i className="fas fa-star fz10 review-color me-2" />
+                <p className="mb-0 body-color fz14">
+                  <span className="dark-color me-2">{rating}</span>
+                  {reviews_total > 1
+                    ? reviews_total + " " + "αξιολογήσεις"
+                    : reviews_total + " " + "αξιολόγηση"}
+                </p>
+              </>
+            )}
+          </div>
         </div>
 
         <hr className="my-2" />
         <div className="list-meta d-flex justify-content-between align-items-center mt15">
-          <UserImage
-            firstName={user.firstName}
-            lastName={user.lastName}
-            displayName={user.displayName}
-            image={user?.image?.data?.attributes?.formats?.thumbnail?.url}
-            alt={user?.image?.formats?.thumbnail?.provider_metadata?.public_id}
-            width={30}
-            height={30}
-          />
+          <div className="archive-service-card-user-meta">
+            <UserImage
+              firstName={user.firstName}
+              lastName={user.lastName}
+              displayName={user.displayName}
+              image={user?.image?.data?.attributes?.formats?.thumbnail?.url}
+              alt={
+                user?.image?.formats?.thumbnail?.provider_metadata?.public_id
+              }
+              width={30}
+              height={30}
+              path={`/profile/${user?.username}`}
+            />
+
+            <VerifiedBadge
+              verified={user.verified}
+              tooltipText="Πιστοποιημένος"
+            />
+            <TopLevelBadge
+              topLevel={freelancer?.data?.attributes?.topLevel}
+              tooltipText="Έχει λάβει εξαιρετικές αξιολογήσεις"
+            />
+          </div>
 
           <div className="budget">
             <p className="mb-0 body-color">
