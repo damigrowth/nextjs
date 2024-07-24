a2f5c275d71af108ca274c11577b3137dc8a96cc
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-24 23:34:53 +0300

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -3,10 +3,13 @@ import { getMaintenanceStatus } from "./lib/maintenance/maintenance";
 import { isAuthenticated } from "./lib/auth/authenticated";
 
 export async function middleware(request) {
+  // Add a new header x-current-path which passes the path to downstream components
+  const headers = new Headers(request.headers);
   const currentPath = request.nextUrl.pathname;
+  headers.set("x-current-path", currentPath);
+
   const { isUnderMaintenance } = await getMaintenanceStatus();
   const { authenticated } = await isAuthenticated();
-
   // List of paths that should be publicly accessible
   const maintenancePublicPaths = [
     "/maintenance",
@@ -46,5 +49,12 @@ export async function middleware(request) {
     }
   }
 
-  return NextResponse.next();
+  return NextResponse.next({ headers });
 }
+
+export const config = {
+  matcher: [
+    // match all routes except static files and APIs
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
