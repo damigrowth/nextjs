ba6af2366da2e655db51baa4f3ac5b2659953f24
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-30 15:42:54 +0300

    Create layout.jsx

---
+++ src/app/(freelancer)/pros/layout.jsx
@@ -0,0 +1,33 @@
+import Tabs from "@/components/ui/Archives/Tabs";
+import Breadcrumb from "@/components/ui/Archives/Breadcrumb";
+import Banner from "@/components/ui/Archives/Banner";
+import { getData } from "@/lib/client/operations";
+import { FREELANCER_CATEGORIES_SEARCH } from "@/lib/graphql/queries";
+import React from "react";
+
+export default async function layout({ children }) {
+  const { freelancerCategories } = await getData(FREELANCER_CATEGORIES_SEARCH);
+
+  return (
+    <>
+      <Tabs
+        parentPathLabel="Όλες οι κατηγορίες"
+        parentPathLink="pros"
+        categories={freelancerCategories?.data}
+        plural
+      />
+      <Breadcrumb
+        parentPathLabel="Επαγγελματίες"
+        parentPathLink="pros"
+        categories={freelancerCategories?.data}
+        plural
+      />
+      <Banner
+        categories={freelancerCategories?.data}
+        heading="Βρείτε Επαγγελματίες και Επιχειρήσεις"
+        description="Ανακαλύψτε και προσλάβετε τις καλύτερες επιχειρήσεις και εξειδικευμένοους επαγγελματίες για οποιαδήποτε ανάγκη."
+      />
+      {children}
+    </>
+  );
+}
