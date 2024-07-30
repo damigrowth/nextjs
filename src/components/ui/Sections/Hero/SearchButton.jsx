0ba9980a95c6873a61ac110b037a885ff512e346
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-30 14:20:48 +0300

    Create SearchButton.jsx

---
+++ src/components/ui/Sections/Hero/SearchButton.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import useHomeStore from "@/store/home/homeStore";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function SearchButton() {
+  const router = useRouter();
+
+  const { searchTerm, categorySelect } = useHomeStore();
+
+  const categorySlug = categorySelect?.attributes?.slug;
+
+  const handleSearch = () => {
+    if (!categorySlug) {
+      router.push(`/ipiresies?search=${searchTerm}`);
+    } else {
+      router.push(`/ipiresies/${categorySlug}?search=${searchTerm}`);
+    }
+  };
+
+  return (
+    <button
+      className="ud-btn btn-dark w-100 bdrs60"
+      type="button"
+      onClick={handleSearch}
+    >
+      Αναζήτηση
+    </button>
+  );
+}
