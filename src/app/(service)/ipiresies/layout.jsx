eb38381e232a0b88f9e03cafc7226550f2941352
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-30 15:10:54 +0300

    Create layout.jsx

---
+++ src/app/(service)/ipiresies/layout.jsx
@@ -0,0 +1,26 @@
+import Banner from "@/components/ui/Archives/Banner";
+import Breadcrumb from "@/components/ui/Archives/Breadcrumb";
+import Tabs from "@/components/ui/Archives/Tabs";
+import { getData } from "@/lib/client/operations";
+import { CATEGORIES } from "@/lib/graphql/queries";
+import React from "react";
+
+export default async function layout({ children }) {
+  const { categories } = await getData(CATEGORIES);
+  return (
+    <>
+      <Tabs
+        parentPathLabel="Όλες οι κατηγορίες"
+        parentPathLink="ipiresies"
+        categories={categories?.data}
+      />
+      <Breadcrumb
+        parentPathLabel="Υπηρεσίες"
+        parentPathLink="ipiresies"
+        categories={categories?.data}
+      />
+      <Banner categories={categories?.data} />
+      {children}
+    </>
+  );
+}
