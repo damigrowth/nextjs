d7301ccbc76ed63429c666d3ffb97b97399f27a9
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-30 14:21:20 +0300

    Create Features.jsx

---
+++ src/components/ui/Sections/Features.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { featuresData } from "../data";
+
+export default function Features() {
+  return (
+    <section className="our-features pb90 pb30-md pt-0">
+      <div className="container wow fadeInUp">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="main-title text-center">
+              <h2>Ψάχνετε κάποια υπηρεσία;</h2>
+              <p className="text">
+                Βρείτε επαγγελματίες που μπορούν να εκτελέσουν την εργασία που
+                θέλετε.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {featuresData.map((feature, index) => (
+            <div key={index} className="col-sm-6 col-lg-3">
+              <div className="iconbox-style1 at-home12 p-0 text-center">
+                <div className="icon before-none">
+                  <span className={feature.iconClass}></span>
+                </div>
+                <div className="details">
+                  <h4 className="title mt10 mb-3">{feature.title}</h4>
+                  <p
+                    className="text"
+                    dangerouslySetInnerHTML={{ __html: feature.description }}
+                  ></p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
