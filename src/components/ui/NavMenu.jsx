21c8867efd6427a6d8ddd0eb51a5c3a7cdbbf184
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-30 14:35:54 +0300

    Create NavMenu.jsx

---
+++ src/components/ui/NavMenu.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+export default function NavMenu() {
+  return (
+    <ul className="ace-responsive-menu ui-navigation">
+      <li className="visible_list menu-active home-menu-parent ">
+        <Link href="/ipiresies" className="list-item">
+          <span>Υπηρεσίες</span>
+        </Link>
+      </li>
+      <li className="visible_list menu-active home-menu-parent">
+        <Link href="/pros" className="list-item">
+          <span className="title">Επαγγελματίες</span>
+        </Link>
+      </li>
+    </ul>
+  );
+}
