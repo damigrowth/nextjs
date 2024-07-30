93af57441cf1a05c4f0461b9323ee92c4fce7723
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-30 15:09:07 +0300

    Create data.js

---
+++ src/components/ui/data.js
@@ -0,0 +1,63 @@
+export const featuresData = [
+  {
+    iconClass: "flaticon-cv",
+    title: "Αναζητήστε Υπηρεσίες",
+    description: `Εντοπίστε την υπηρεσία που ικανοποιεί τις ανάγκες σας.`,
+  },
+  {
+    iconClass: "flaticon-web-design",
+    title: "Επιλέξτε Επαγγελματίες",
+    description: `Με τη Doulitsa είναι πολύ εύκολο να εντοπίσετε τον κατάλληλο επαγγελματία.`,
+  },
+  {
+    iconClass: "flaticon-secure",
+    title: "Τρόποι Πληρωμής",
+    description: `Επιλέξτε τον τρόπο πληρωμής που σας δίνει τη μεγαλύτερη ασφάλεια.`,
+  },
+  {
+    iconClass: "flaticon-customer-service",
+    title: "Επικοινωνία",
+    description: `Διαλέξτε εσείς με ποιον τρόπο θέλετε να επικοινωνήσετε.`,
+  },
+];
+
+export const testimonials = [
+  {
+    id: 1,
+    title: "Σας συστήνω ανεπιφύλακτα",
+    comment:
+      "“Η επιχείρηση μου είναι μια μικρή ξενοδοχειακή μονάδα. Η Doulitsa με βοηθάει να βρω επαγγελματίες που δεν μπορούσα να προσλάβω σε μόνιμη θέση.”",
+    image: "/images/testimonials/testimonial-1.png",
+    name: "Δημήτρης Κ.",
+    category: "Ξενοδόχος",
+  },
+  {
+    id: 2,
+    title: "Πολύ καλή δουλειά",
+    comment:
+      "“Βρήκα γρήγορα έναν επαγγελματία designer για να μου σχεδιάσει ένα καινούργιο logo.”",
+    image: "/images/testimonials/testimonial-2.png",
+    name: "Manolis S.",
+    category: "Επιχειρηματίας",
+  },
+  {
+    id: 3,
+    title: "Είμαι πολύ ικανοποιημένος",
+    comment:
+      "“Μέσα από την πλατφόρμα μπόρεσα να παρουσιάσω όλη τη δουλειά μου και να βρω νέους πελάτες, αλλά και συνεργάτες.”",
+    image: "/images/testimonials/testimonial-3.png",
+    name: "Courtney Henry",
+    category: "Web Developer",
+  },
+];
+
+export const categoriesClassNames = [
+  "col-sm-6 col-lg-4 col-xl-3 bdr1 m--1 border-start-0 border-top-0",
+  "col-sm-6 col-lg-4 col-xl-3 bdr1 m--1 border-top-0",
+  "col-sm-6 col-lg-4 col-xl-3 bdr1 m--1 border-top-0",
+  "col-sm-6 col-lg-4 col-xl-3 m--1 border-start-0 border-bottom-0",
+  "col-sm-6 col-lg-4 col-xl-3 bdr1 m--1 border-start-0 border-bottom-0",
+  "col-sm-6 col-lg-4 col-xl-3 bdr1 m--1 border-bottom-0",
+  "col-sm-6 col-lg-4 col-xl-3 bdr1 m--1 border-bottom-0 border-end-0",
+  "col-sm-6 col-lg-4 col-xl-3 bdr1 m--1 border-bottom-0 border-end-0",
+];
