f9e748fb7a71bf11b68b97ea19c577ef41bdd560
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-30 15:10:08 +0300

    Update Breadcrumb.jsx

--- src/components/ui/Archives/Breadcrumb.jsx
+++ src/components/ui/Archives/Breadcrumb.jsx
@@ -1,17 +1,30 @@
+"use client";
+
+import { inspect } from "@/utils/inspect";
+import { getPathname } from "@/utils/paths";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function Breadcrumb({
   parentPathLabel,
   parentPathLink,
-  category,
   categories,
   plural,
 }) {
+  const pathName = usePathname();
+  const category = getPathname(pathName, 1);
+  const subcategory = getPathname(pathName, 2);
+
   // Find the current category from the array of categories
   const currentCategory = categories.find(
     (cat) => cat.attributes.slug === category
   );
+
+  const currentSubcategory =
+    currentCategory?.attributes?.subcategories?.data?.find(
+      (sub) => sub.attributes.slug === subcategory
+    );
 
   return (
     <section className="breadcumb-section">
@@ -22,11 +35,20 @@ export default function Breadcrumb({
               <div className="breadcumb-list">
                 <Link href="/">Αρχική</Link>
                 <Link href={`/${parentPathLink}`}>{parentPathLabel}</Link>
-                {category && (
-                  <div className="archive-breadcrump-active">
+                {currentCategory && (
+                  <Link
+                    href={`/${parentPathLink}/${currentCategory.attributes.slug}`}
+                  >
                     {plural
                       ? currentCategory.attributes.plural
                       : currentCategory.attributes.label}
+                  </Link>
+                )}
+                {currentSubcategory && (
+                  <div className="archive-breadcrump-active">
+                    {plural
+                      ? currentSubcategory.attributes.plural
+                      : currentSubcategory.attributes.label}
                   </div>
                 )}
               </div>
