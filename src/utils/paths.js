5e7b01444032cda3d2d2cfd7a78b8a48e78b969d
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-30 14:18:35 +0300

    Create paths.js

---
+++ src/utils/paths.js
@@ -0,0 +1,9 @@
+export const getPathSegments = (path) => {
+  return path.split("/").filter(Boolean);
+};
+
+export const getPathname = (path, index) => {
+  const segments = getPathSegments(path);
+
+  return segments[index];
+};
