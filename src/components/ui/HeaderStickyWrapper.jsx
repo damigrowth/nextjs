d40981f42b4d2d5fafc8b2a3f03dae91254bb891
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-30 14:35:59 +0300

    Create HeaderStickyWrapper.jsx

---
+++ src/components/ui/HeaderStickyWrapper.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import { usePathname } from "next/navigation";
+import useStickyMenu from "@/hook/useStickyMenu";
+
+export default function HeaderStickyWrapper({ children }) {
+  const sticky = useStickyMenu(70);
+  const pathname = usePathname();
+
+  return (
+    <header
+      className={`header-nav ${
+        pathname === "/" ? "nav-homepage-style" : ""
+      } at-home3 stricky main-menu border-0 ${
+        sticky ? "slideInDown stricky-fixed" : "slideIn"
+      }`}
+    >
+      {children}
+    </header>
+  );
+}
