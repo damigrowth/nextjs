d54bc611b33fcf8748a2477d2c09418022260a9e
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-30 14:35:47 +0300

    Create FeaturedCategories.jsx

---
+++ src/components/ui/Sections/Featured/Categories/FeaturedCategories.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+// import BrowserCategoryCard1 from "../card/BrowserCategoryCard1";
+import Link from "next/link";
+import { getData } from "@/lib/client/operations";
+import { FEATURED_CATEGORIES } from "@/lib/graphql/queries";
+import { categoriesClassNames } from "../../../data";
+import FeaturedCategoriesSwiper from "./FeaturedCategoriesSwiper";
+
+export default async function FeaturedCategories({ categories }) {
+  const featuredCategories = categories.map((item, i) => ({
+    ...item,
+    classNames: categoriesClassNames[i % categoriesClassNames.length],
+  }));
+
+  return (
+    <section className="pb40-md pb90">
+      <div className="container">
+        <div
+          className="row align-items-center wow fadeInUp"
+          data-wow-delay="300ms"
+        >
+          <div className="col-lg-9">
+            <div className="main-title2">
+              <h2 className="title">Κατηγορίες</h2>
+              <p className="paragraph">Ανακαλύψτε 100+ κατηγορίες υπηρεσιών.</p>
+            </div>
+          </div>
+          <div className="col-lg-3">
+            <div className="text-start text-lg-end mb-4 mb-lg-2">
+              <Link className="ud-btn2" href="/ipiresies">
+                Όλες οι Κατηγορίες
+                <i className="fal fa-arrow-right-long"></i>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="row d-none d-lg-flex wow fadeInUp">
+          {featuredCategories.map((item, i) => (
+            <div key={i} className={item.classNames}>
+              <div className="iconbox-style1 at-home12-v2">
+                <div className="icon">
+                  <span className={item.attributes.icon}></span>
+                </div>
+                <div className="details mt20">
+                  {/* <p className="text mb5">{item.skills} skills</p> */}
+                  <h4 className="title">
+                    <Link href={`/ipiresies/${item.attributes.slug}`}>
+                      {item.attributes.label}
+                    </Link>
+                  </h4>
+                  <p className="mb-0">
+                    {item.attributes.subcategories.data
+                      .slice(0, 3)
+                      .map((sub) => sub.attributes.label)
+                      .join(", ")}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <FeaturedCategoriesSwiper categories={featuredCategories} />
+      </div>
+    </section>
+  );
+}
