216c180b42549222f02761e9dbeeb996d88b6ced
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-07-30 15:10:52 +0300

    Update page.jsx

--- src/app/(service)/ipiresies/page.jsx
+++ src/app/(service)/ipiresies/page.jsx
@@ -1,12 +1,4 @@
-import Breadcumb3 from "@/components/breadcumb/Breadcumb3";
-import Breadcumb6 from "@/components/breadcumb/Breadcumb6";
-
-import Listing4 from "@/components/section/Listing4";
-import TabSection1 from "@/components/section/TabSection1";
-import BannerVid from "@/components/ui/Archives/BannerVid";
-import Breadcrumb from "@/components/ui/Archives/Breadcrumb";
 import ServicesArchive from "@/components/ui/Archives/Services/ServicesArchive";
-import Tabs from "@/components/ui/Archives/Tabs";
 import { getData } from "@/lib/client/operations";
 import { CATEGORIES_SEARCH, SERVICES_ARCHIVE } from "@/lib/graphql/queries";
 
@@ -15,11 +7,12 @@ export const metadata = {
 };
 
 export default async function page({ searchParams }) {
-  const { min, max, time, cat, cat_s, ver, page, sort } = searchParams;
+  const { search, min, max, time, cat, cat_s, ver, page, sort } = searchParams;
 
   const addFilter = (condition, value) => (condition ? value : undefined);
 
   const paramsFilters = {
+    search: search || undefined,
     min: addFilter(min, parseInt(min, 10)),
     max: addFilter(max, parseInt(max, 10)),
     time: addFilter(time, parseInt(time, 10)),
@@ -37,21 +30,6 @@ export default async function page({ searchParams }) {
 
   return (
     <>
-      <Tabs
-        parentPathLabel="Όλες οι κατηγορίες"
-        parentPathLink="ipiresies"
-        categories={categories?.data}
-        searchParams={searchParams}
-      />
-      <Breadcrumb
-        parentPathLabel="Υπηρεσίες"
-        parentPathLink="ipiresies"
-        categories={categories?.data}
-      />
-      <BannerVid
-        heading="Όλες οι Υπηρεσίες"
-        description="Ανακαλύψτε τις υπηρεσίες που χρειάζεστε απο τους επαγγελματίες μας."
-      />
       <ServicesArchive
         categories={categories?.data}
         searchParams={searchParams}
