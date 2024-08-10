7c72764271e73c0844ece6cfcb802b85f0efa311
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-08-10 18:29:55 +0300

    ML4-88 Range Slider

--- src/components/ui/Archives/Freelancers/Filters/Rate.jsx
+++ src/components/ui/Archives/Freelancers/Filters/Rate.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import RangeSlider from "../../Inputs/RangeSlider";
 
 export default function Rate() {
-  return <RangeSlider iniMin={5} iniMax={500} />;
+  return <RangeSlider iniMin={5} iniMax={100} />;
 }
