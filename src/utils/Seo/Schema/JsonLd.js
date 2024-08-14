7cccdb0b73ae891f3c5c36d87b57ec14f581f4ee
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-08-14 19:11:09 +0300

    ML4-134 JsonLd Schema Component

---
+++ src/utils/Seo/Schema/JsonLd.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const JsonLd = ({ data }) => {
+  const jsonData = JSON.stringify(data);
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: jsonData }}
+    />
+  );
+};
+
+export default JsonLd;
