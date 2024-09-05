70df97a171acd52f8ecdd5ed17917cde226fe4d5
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-09-05 20:24:12 +0300

    ML4-147 Fix Boolean Switch Field

--- src/app/(archives)/ipiresies/page.jsx
+++ src/app/(archives)/ipiresies/page.jsx
@@ -29,7 +29,7 @@ export default async function page({ searchParams }) {
     max: addFilter(max, parseInt(max, 10)),
     time: addFilter(time, parseInt(time, 10)),
     cat: addFilter(cat, parseInt(cat, 10)),
-    verified: addFilter(ver, ver === "true"),
+    verified: addFilter(ver === "", true),
     page: !page || parseInt(page, 10) < 1 ? 1 : parseInt(page, 10),
     sort: sort ? sort : "publishedAt:desc",
   };
