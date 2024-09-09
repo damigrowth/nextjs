fb12e18cc434c9417c1d782d29a5068dfd36c7be
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-09-09 18:22:48 +0300

    ML4-107 Single Service Skeleton

--- src/app/(service)/s/[slug]/loading.jsx
+++ src/app/(service)/s/[slug]/loading.jsx
@@ -1,9 +1,6 @@
+import SingleServiceSkeleton from "@/components/ui/Skeletons/SingleServiceSkeleton";
 import React from "react";
 
 export default function loading() {
-  return (
-    <div>
-      <h2>loading...</h2>
-    </div>
-  );
+  return <SingleServiceSkeleton />;
 }
