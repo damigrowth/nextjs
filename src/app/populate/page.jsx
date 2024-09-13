2be51dac76b475df3d4cf1f83d1ef6a1b8c813e9
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-09-14 00:40:57 +0300

    ML4-172 Make populateTaxonomies Dynamic

--- src/app/populate/page.jsx
+++ src/app/populate/page.jsx
@@ -3,10 +3,26 @@ import populateTaxonomies from "@/lib/populate/taxonomies";
 import React from "react";
 
 export default async function page() {
-  const endpoint = "freelancer-subcategories";
-  const filePath = "src/lib/populate/taxonomies/freelancer_subcategories.csv";
+  const limit = 0;
 
-  await populateTaxonomies(0, endpoint, filePath, 0);
+  // const endpoint = "freelancer-subcategories";
+  // const filePath = "src/lib/populate/taxonomies/freelancer_subcategories.csv";
+
+  // const endpoint = "subcategories";
+  // const filePath = "src/lib/populate/taxonomies/services_subcategories.csv";
+
+  const endpoint = "subdivisions";
+  const filePath = "src/lib/populate/taxonomies/services_subdivisions.csv";
+
+  await populateTaxonomies(
+    0,
+    endpoint,
+    filePath,
+    limit,
+    ["label", "subcategory", "category", "slug"],
+    "subcategory"
+  );
+
   // await findColumnIds("subdivisions");
   return <div>page</div>;
 }
