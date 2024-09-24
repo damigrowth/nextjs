8390f9ddca0c451457056045b6429c061142259b
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-09-24 19:24:23 +0300

    ML5-31 Extra Υπηρεσίες - clickable

--- src/components/ui/SingleService/Addons.jsx
+++ src/components/ui/SingleService/Addons.jsx
@@ -36,7 +36,7 @@ export default function Addons({ addons, small, price }) {
                 key={i}
                 className={`${
                   !small
-                    ? `nav-link ${
+                    ? `nav-link p0 ${
                         order.addons?.some((a) => a.id === addon.id)
                           ? "active"
                           : ""
@@ -45,10 +45,7 @@ export default function Addons({ addons, small, price }) {
                 }`}
               >
                 {!small ? (
-                  <label
-                    className="custom_checkbox fw500 text-start"
-                    style={{ paddingLeft: "30px" }}
-                  >
+                  <label className="custom_checkbox fw500 text-start m0">
                     <h5 className="small-addon-title">{addon.title}</h5>
                     <span className="text text-bottom">
                       {addon.description}
