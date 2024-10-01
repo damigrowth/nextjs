6892f8eadf79ce092a2fa932f133958a79464c85
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-10-02 00:47:22 +0300

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,19 +4,8 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 import BottomToTop from "@/components/button/BottomToTop";
 import Header from "@/components/ui/Header";
-import SearchModal1 from "@/components/modal/SearchModal1";
-import { footer } from "@/data/footer";
-import { headers } from "next/headers";
-// import { getMaintenanceStatus } from "@/lib/maintenance/maintenance";
-import { isAuthenticated } from "@/lib/auth/authenticated";
 import InstallBootstrap from "@/components/ui/InstallBootstrap";
 import Body from "@/components/ui/Body";
-import Footer from "@/components/ui/Footer";
-import { getUser } from "@/lib/user/user";
-import NavMenuMobile from "@/components/ui/NavMenuMobile";
-// import { checkServerHealth, getData } from "@/lib/client/operations";
-import ServerDown from "@/components/ui/Errors/ServerDown";
-import { GoogleAnalytics } from "@next/third-parties/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Query from "@/components/query/Query";
 import { ROOT_LAYOUT } from "@/lib/graphql/queries/main/global";
@@ -26,29 +15,6 @@ if (typeof window !== "undefined") {
 }
 
 export default async function RootLayout({ children }) {
-  // const { serverStatus } = await checkServerHealth();
-
-  // if (!serverStatus)
-  //   return (
-  //     <html>
-  //       <body>
-  //         <ServerDown />
-  //       </body>
-  //     </html>
-  //   );
-
-  // const headerList = headers();
-  // const path = headerList.get("x-current-path");
-
-  const path = true;
-
-  // const { isUnderMaintenance } = await getMaintenanceStatus();
-  const isUnderMaintenance = false;
-  const { authenticated } = await isAuthenticated();
-  const user = await getUser();
-
-  const gaId = process.env.GA_ID;
-
   return (
     <html lang="el">
       <Body>
@@ -56,37 +22,15 @@ export default async function RootLayout({ children }) {
         <Query query={ROOT_LAYOUT}>
           {(data) => (
             <>
-              {path ? (
-                <div className="wrapper ovh mm-page mm-slideout">
-                  {(!isUnderMaintenance || authenticated) && (
-                    <Header
-                      authenticated={authenticated}
-                      user={user}
-                      header={data.header}
-                    />
-                  )}
-                  {/* <SearchModal1 /> */}
-                  <div className="body_content">
-                    {children}
-                    {(!isUnderMaintenance || authenticated) && (
-                      <Footer footer={data.footer} />
-                    )}
-                    <BottomToTop />
-                  </div>
-                </div>
-              ) : (
-                <div className="wrapper mm-page mm-slideout">
-                  {children}
-                  <BottomToTop />
-                </div>
-              )}
-              {/* <NavMenuMobile header={headerData} /> */}
+              <Header header={data.header} />
+              <div className="wrapper mm-page mm-slideout">
+                {children}
+                <BottomToTop />
+              </div>
             </>
           )}
         </Query>
-
         <SpeedInsights />
-        <GoogleAnalytics gaId={gaId} />
       </Body>
     </html>
   );
