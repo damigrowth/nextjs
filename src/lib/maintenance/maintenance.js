7611fc3f760ab4ec8e95bc4526b107f6769b510f
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-10-01 20:27:14 +0300

    Logging Maintenance status

--- src/lib/maintenance/maintenance.js
+++ src/lib/maintenance/maintenance.js
@@ -2,6 +2,7 @@ import { getData } from "../client/operations";
 import { MAINTENANCE_STATUS } from "../graphql/queries/main/global";
 
 export async function getMaintenanceStatus() {
+  console.log("Checking maintenance status...");
   const { maintenance } = await getData(MAINTENANCE_STATUS);
 
   const isUnderMaintenance = maintenance?.data?.attributes?.isActive;
