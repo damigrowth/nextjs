d0523ce982fd7db1e4e7c89ea3b0586265b700ea
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-10-07 16:06:48 +0300

    ML5-87 Service archive broken

--- src/components/ui/Reviews/CardReviews.jsx
+++ src/components/ui/Reviews/CardReviews.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function CardReviews({ rating, reviews_total }) {
   return (
     <div className="review-meta d-flex align-items-center">
-      {reviews_total && reviews_total > 0 && (
+      {reviews_total > 0 && reviews_total > 0 && (
         <>
           <i className="fas fa-star fz10 review-color me-2" />
           <p className="mb-0 body-color fz14">
