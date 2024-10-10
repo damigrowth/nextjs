4a443acf4840cc151f4960ae5546024f3921584f
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-10-10 18:02:34 +0300

    ML5-16 Terms - Pages

--- src/app/(pages)/terms/page.jsx
+++ src/app/(pages)/terms/page.jsx
@@ -1,22 +1,13 @@
-import Terms from "@/components/ui/Pages/Terms";
-import { getData } from "@/lib/client/operations";
-import { GET_PAGE_BY_SLUG } from "@/lib/graphql/queries/main/page";
 import React from "react";
+import PageContent from "@/components/ui/Pages/PageContent";
+import { data } from "@/data/pages/terms";
 
 export default async function TermsPage() {
-  const { pages: res } = await getData(GET_PAGE_BY_SLUG, {
-    slug: "terms",
-  });
-
-  const page = res.data[0].attributes;
-
   return (
-    <div>
-      <Terms
-        title={page?.title}
-        description={page.description}
-        tabs={page.tabs}
-      />
-    </div>
+    <PageContent
+      title={data.title}
+      description={data.description}
+      tabs={data.tabs}
+    />
   );
 }
---
+++ src/data/pages/terms.js
@@ -0,0 +1,63 @@
+export const data = {
+  title: "Όροι Χρήσης",
+  description: "Περιγραφή της σελίδας",
+  tabs: [
+    {
+      title: "Καρτέλα 1",
+      content: [
+        {
+          heading: "Τίτλος 1",
+          paragraph:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus nascetur morbi nisl, mi, in semper metus porttitor non. Augue nunc amet fringilla sit. Fringilla eget arcu sodales sed a, parturient fermentum amet scelerisque. Amet purus urna, dictumst aliquet aliquam natoque non, morbi pretium. Integer amet fermentum nibh viverra mollis consectetur arcu, ultrices dolor. Gravida purus arcu viverra eget. Aliquet tincidunt dignissim aliquam tempor nec id. Habitant suscipit sit semper duis odio amet, at.\n" +
+            "\n" +
+            "Massa ultricies a arcu velit eget gravida purus ultrices eget. Orci, fames eu facilisi justo. Lacus netus a at sed justo vel leo leo pellentesque. Nulla ut laoreet luctus cum turpis et amet ac viverra. Vitae neque orci dui eu ac tincidunt. Egestas placerat egestas netus nec velit gravida consectetur laoreet vitae. Velit sed enim habitant habitant non diam. Semper tellus turpis tempus ac leo tempor. Ultricies amet, habitasse adipiscing bibendum consequat amet, sed. Id convallis suspendisse vitae, lacinia mattis cursus montes, dui. Tortor lobortis dignissim eget egestas. Eget enim auctor nunc eget mattis sollicitudin senectus diam. Tincidunt morbi egestas dignissim eget id aliquam.",
+        },
+        {
+          heading: "Τίτλος 2",
+          paragraph:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus nascetur morbi nisl, mi, in semper metus porttitor non. Augue nunc amet fringilla sit. Fringilla eget arcu sodales sed a, parturient fermentum amet scelerisque. Amet purus urna, dictumst aliquet aliquam natoque non, morbi pretium. Integer amet fermentum nibh viverra mollis consectetur arcu, ultrices dolor. Gravida purus arcu viverra eget. Aliquet tincidunt dignissim aliquam tempor nec id. Habitant suscipit sit semper duis odio amet, at.\n" +
+            "\n" +
+            "Massa ultricies a arcu velit eget gravida purus ultrices eget. Orci, fames eu facilisi justo. Lacus netus a at sed justo vel leo leo pellentesque. Nulla ut laoreet luctus cum turpis et amet ac viverra. Vitae neque orci dui eu ac tincidunt. Egestas placerat egestas netus nec velit gravida consectetur laoreet vitae. Velit sed enim habitant habitant non diam. Semper tellus turpis tempus ac leo tempor. Ultricies amet, habitasse adipiscing bibendum consequat amet, sed. Id convallis suspendisse vitae, lacinia mattis cursus montes, dui. Tortor lobortis dignissim eget egestas. Eget enim auctor nunc eget mattis sollicitudin senectus diam. Tincidunt morbi egestas dignissim eget id aliquam.",
+        },
+      ],
+    },
+    {
+      title: "Καρτέλα 2",
+      content: [
+        {
+          heading: "Τίτλος 1",
+          paragraph:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus nascetur morbi nisl, mi, in semper metus porttitor non. Augue nunc amet fringilla sit. Fringilla eget arcu sodales sed a, parturient fermentum amet scelerisque. Amet purus urna, dictumst aliquet aliquam natoque non, morbi pretium. Integer amet fermentum nibh viverra mollis consectetur arcu, ultrices dolor. Gravida purus arcu viverra eget. Aliquet tincidunt dignissim aliquam tempor nec id. Habitant suscipit sit semper duis odio amet, at.\n" +
+            "\n" +
+            "Massa ultricies a arcu velit eget gravida purus ultrices eget. Orci, fames eu facilisi justo. Lacus netus a at sed justo vel leo leo pellentesque. Nulla ut laoreet luctus cum turpis et amet ac viverra. Vitae neque orci dui eu ac tincidunt. Egestas placerat egestas netus nec velit gravida consectetur laoreet vitae. Velit sed enim habitant habitant non diam. Semper tellus turpis tempus ac leo tempor. Ultricies amet, habitasse adipiscing bibendum consequat amet, sed. Id convallis suspendisse vitae, lacinia mattis cursus montes, dui. Tortor lobortis dignissim eget egestas. Eget enim auctor nunc eget mattis sollicitudin senectus diam. Tincidunt morbi egestas dignissim eget id aliquam.",
+        },
+        {
+          heading: "Τίτλος 2",
+          paragraph:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus nascetur morbi nisl, mi, in semper metus porttitor non. Augue nunc amet fringilla sit. Fringilla eget arcu sodales sed a, parturient fermentum amet scelerisque. Amet purus urna, dictumst aliquet aliquam natoque non, morbi pretium. Integer amet fermentum nibh viverra mollis consectetur arcu, ultrices dolor. Gravida purus arcu viverra eget. Aliquet tincidunt dignissim aliquam tempor nec id. Habitant suscipit sit semper duis odio amet, at.\n" +
+            "\n" +
+            "Massa ultricies a arcu velit eget gravida purus ultrices eget. Orci, fames eu facilisi justo. Lacus netus a at sed justo vel leo leo pellentesque. Nulla ut laoreet luctus cum turpis et amet ac viverra. Vitae neque orci dui eu ac tincidunt. Egestas placerat egestas netus nec velit gravida consectetur laoreet vitae. Velit sed enim habitant habitant non diam. Semper tellus turpis tempus ac leo tempor. Ultricies amet, habitasse adipiscing bibendum consequat amet, sed. Id convallis suspendisse vitae, lacinia mattis cursus montes, dui. Tortor lobortis dignissim eget egestas. Eget enim auctor nunc eget mattis sollicitudin senectus diam. Tincidunt morbi egestas dignissim eget id aliquam.",
+        },
+      ],
+    },
+    {
+      title: "Καρτέλα 3",
+      content: [
+        {
+          heading: "Τίλτος 3",
+          paragraph:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus nascetur morbi nisl, mi, in semper metus porttitor non. Augue nunc amet fringilla sit. Fringilla eget arcu sodales sed a, parturient fermentum amet scelerisque. Amet purus urna, dictumst aliquet aliquam natoque non, morbi pretium. Integer amet fermentum nibh viverra mollis consectetur arcu, ultrices dolor. Gravida purus arcu viverra eget. Aliquet tincidunt dignissim aliquam tempor nec id. Habitant suscipit sit semper duis odio amet, at.\n" +
+            "\n" +
+            "Massa ultricies a arcu velit eget gravida purus ultrices eget. Orci, fames eu facilisi justo. Lacus netus a at sed justo vel leo leo pellentesque. Nulla ut laoreet luctus cum turpis et amet ac viverra. Vitae neque orci dui eu ac tincidunt. Egestas placerat egestas netus nec velit gravida consectetur laoreet vitae. Velit sed enim habitant habitant non diam. Semper tellus turpis tempus ac leo tempor. Ultricies amet, habitasse adipiscing bibendum consequat amet, sed. Id convallis suspendisse vitae, lacinia mattis cursus montes, dui. Tortor lobortis dignissim eget egestas. Eget enim auctor nunc eget mattis sollicitudin senectus diam. Tincidunt morbi egestas dignissim eget id aliquam.",
+        },
+        {
+          heading: "Τίτλος 5",
+          paragraph:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus nascetur morbi nisl, mi, in semper metus porttitor non. Augue nunc amet fringilla sit. Fringilla eget arcu sodales sed a, parturient fermentum amet scelerisque. Amet purus urna, dictumst aliquet aliquam natoque non, morbi pretium. Integer amet fermentum nibh viverra mollis consectetur arcu, ultrices dolor. Gravida purus arcu viverra eget. Aliquet tincidunt dignissim aliquam tempor nec id. Habitant suscipit sit semper duis odio amet, at.\n" +
+            "\n" +
+            "Massa ultricies a arcu velit eget gravida purus ultrices eget. Orci, fames eu facilisi justo. Lacus netus a at sed justo vel leo leo pellentesque. Nulla ut laoreet luctus cum turpis et amet ac viverra. Vitae neque orci dui eu ac tincidunt. Egestas placerat egestas netus nec velit gravida consectetur laoreet vitae. Velit sed enim habitant habitant non diam. Semper tellus turpis tempus ac leo tempor. Ultricies amet, habitasse adipiscing bibendum consequat amet, sed. Id convallis suspendisse vitae, lacinia mattis cursus montes, dui. Tortor lobortis dignissim eget egestas. Eget enim auctor nunc eget mattis sollicitudin senectus diam. Tincidunt morbi egestas dignissim eget id aliquam.",
+        },
+      ],
+    },
+  ],
+};
