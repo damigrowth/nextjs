6bae7b73512c9b349c462cd1adf916bd858797c7
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-10-10 18:04:12 +0300

    Create faq.js

---
+++ src/data/pages/faq.js
@@ -0,0 +1,20 @@
+export const data = {
+  title: "Συχνές Ερωτήσεις",
+  description: "Η περιγραφή της σελίδας",
+  faq: [
+    {
+      title: "Ερωτήσεις 1",
+      faq: [
+        { question: "Ερώτηση 1", answer: "Απάντηση 1" },
+        { question: "Ερώτηση 2", answer: "Απάντηση 2" },
+      ],
+    },
+    {
+      title: "Ερωτήσεις 2",
+      faq: [
+        { question: "Ερώτηση 3", answer: "Απάντηση 3" },
+        { question: "Ερώτηση 4", answer: "Απάντηση 4" },
+      ],
+    },
+  ],
+};
