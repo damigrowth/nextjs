e5fbc67a87e324353af5ce57101cd1ba70bd80c1
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-10-15 20:27:24 +0300

    ML5-109 Graphql Client-Side Fetching With Api Route - General

---
+++ src/lib/client/apollo-wrapper.js
@@ -0,0 +1,43 @@
+"use client";
+
+import { ApolloLink, HttpLink } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  InMemoryCache,
+  ApolloClient,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support";
+import { STRAPI_GRAPHQL, STRAPI_TOKEN } from "../strapi";
+
+function makeClient() {
+  console.log("ENV VARS", STRAPI_GRAPHQL, STRAPI_TOKEN);
+
+  const httpLink = new HttpLink({
+    uri: "/api/graphql",
+    // headers: {
+    //   Authorization: `Bearer ${STRAPI_TOKEN}`,
+    // },
+    // fetchOptions: { cache: "no-store" },
+  });
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+export function ApolloWrapper({ children }) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
