caa08d9ffef3560c51b0ac5908d172bdbb9f978e
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-10-26 21:19:41 +0300

    Fixing Build

--- src/lib/strapi.js
+++ src/lib/strapi.js
@@ -1,13 +1,13 @@
 // Strapi Url and Token
-export const STRAPI_URL = process.env.STRAPI_API_URL;
+export const STRAPI_URL = process.env.STRAPI_URL;
 export const STRAPI_TOKEN = process.env.STRAPI_API_TOKEN;
 export const STRAPI_GRAPHQL = process.env.STRAPI_GRAPHQL_URL;
 
 export const validateEnvVars = () => {
   // Check for required environment variables
-  if (!STRAPI_URL || !STRAPI_TOKEN) {
+  if (!STRAPI_GRAPHQL || !STRAPI_TOKEN) {
     throw new Error(
-      "Missing STRAPI_API_URL or STRAPI_API_TOKEN environment variable."
+      "Missing STRAPI_URL or STRAPI_API_TOKEN environment variable."
     );
   }
 };
