da1497d91c36a8a4c9d78283d6e3372f4f118d8a
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-10-26 19:26:52 +0300

    Update TaxonomiesArchive.jsx

--- src/components/ui/Archives/Taxonomies/TaxonomiesArchive.jsx
+++ src/components/ui/Archives/Taxonomies/TaxonomiesArchive.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import TaxonomiesSwiper from "./TaxonomiesSwiper";
 import TaxonomiesGrid from "./TaxonomiesGrid";
@@ -7,11 +5,11 @@ import TaxonomiesGrid from "./TaxonomiesGrid";
 export default function TaxonomiesArchive({ archive }) {
   return (
     <div className="taxonomies-archive">
-      {subdivisions.length > 0 && (
-        <TaxonomiesSwiper taxonomies={archive?.subdivisions} />
+      {archive.subdivisions && archive.subdivisions.length > 0 && (
+        <TaxonomiesSwiper taxonomies={archive.subdivisions} />
       )}
-      {subcategories.length > 0 && (
-        <TaxonomiesGrid taxonomies={archive?.subcategories} />
+      {archive.subcategories && archive.subcategories.length > 0 && (
+        <TaxonomiesGrid taxonomies={archive.subcategories} />
       )}
     </div>
   );
