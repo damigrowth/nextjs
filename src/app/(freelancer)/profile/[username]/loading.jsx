62c9082520ad114f7c0c2d1b7630382ce6e9cd3e
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-11-01 16:25:50 +0200

    ML6-8 Profile Page Loading - Loading

---
+++ src/app/(freelancer)/profile/[username]/loading.jsx
@@ -0,0 +1,6 @@
+import FreelancerProfileSkeleton from "@/components/ui/Skeletons/FreelancerProfileSkeleton";
+import React from "react";
+
+export default function loading() {
+  return <FreelancerProfileSkeleton />;
+}
