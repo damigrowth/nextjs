8bb6c5caf88ea4421eb5a2608a58ed1ce109ee47
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-12-10 02:31:56 +0200

    Force Dynamic /dashboard/reviews

--- src/app/(dashboard)/dashboard/reviews/page.jsx
+++ src/app/(dashboard)/dashboard/reviews/page.jsx
@@ -6,6 +6,10 @@ export const metadata = {
   title: "Αξιολογήσεις | Doulitsa",
 };
 
+export const dynamic = "force-dynamic";
+export const revalidate = 3600;
+// export const dynamicParams = true;
+
 export default function page() {
   return <ReviewsInfo />;
 }
