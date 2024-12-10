45745cf388a7672bb8f93dc31a30b86304079597
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-12-10 17:01:10 +0200

    Update token.js

--- src/lib/auth/token.js
+++ src/lib/auth/token.js
@@ -1,7 +1,8 @@
-import { cookies } from "next/headers";
+import { getCookieData } from "@/utils/cookies";
 
 export async function getAuthToken() {
-  const cookieStore = await cookies();
-  const authToken = cookieStore.get("jwt")?.value;
+  const cookieData = await getCookieData();
+  const jwtCookie = cookieData.find((cookie) => cookie.name === "jwt");
+  const authToken = jwtCookie ? jwtCookie.value : undefined;
   return authToken;
 }
