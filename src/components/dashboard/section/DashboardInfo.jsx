4ebc3fc5c1c6e5e72524a306712b774bbb2f62ba
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2025-01-19 21:02:59 +0200

    ML6-46 Dashboard page - Auth & Dashboard

--- src/components/dashboard/section/DashboardInfo.jsx
+++ src/components/dashboard/section/DashboardInfo.jsx
@@ -10,25 +10,38 @@ import {
   POPULAR_SERVICES_DASHBOARD,
 } from "@/lib/graphql/queries/main/dashboard";
 import Link from "next/link";
-import { getFreelancerId } from "@/lib/users/freelancer";
+import { getAccess, getUser } from "@/lib/auth/user";
 
 export default async function DashboardInfo() {
-  const { fid } = await getFreelancerId();
+  const user = await getUser();
 
-  const { services } = await getData(ALL_SERVICES_DASHBOARD, {
-    id: fid,
-  });
+  const hasAccess = await getAccess(["freelancer", "company"]);
 
-  const { reviews } = await getData(ALL_REVIEWS_RECEIVED_DASHBOARD, {
-    id: fid,
-  });
+  const freelancerId = user.freelancer.data.id;
 
-  const popularServices = await getData(POPULAR_SERVICES_DASHBOARD, {
-    id: fid,
-  });
+  let data = {};
 
-  const totalServices = services?.meta?.pagination?.total || 0;
-  const totalReviews = reviews?.meta?.pagination?.total || 0;
+  if (hasAccess) {
+    const { services } = await getData(ALL_SERVICES_DASHBOARD, {
+      id: freelancerId,
+    });
+
+    const { reviews } = await getData(ALL_REVIEWS_RECEIVED_DASHBOARD, {
+      id: freelancerId,
+    });
+
+    const popularServices = await getData(POPULAR_SERVICES_DASHBOARD, {
+      id: freelancerId,
+    });
+
+    data = {
+      services,
+      reviews,
+      popularServices,
+      totalServices: services?.meta?.pagination?.total || 0,
+      totalReviews: reviews?.meta?.pagination?.total || 0,
+    };
+  }
 
   return (
     <>
@@ -39,88 +52,104 @@ export default async function DashboardInfo() {
           </div>
           <div className="col-lg-12">
             <div className="dashboard_title_area">
-              <h2>Πίνακας Ελέγχου</h2>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-6 col-xxl-3">
-            <div className="d-flex align-items-center justify-content-between statistics_funfact">
-              <div className="details">
-                <div className="fz15">Υπηρεσίες</div>
-                <div className="title">{totalServices}</div>
-                {/* <div className="text fz14">
-                  <span className="text-thm">10</span> New Offered
-                </div> */}
-              </div>
-              <div className="icon text-center">
-                <i className="flaticon-contract" />
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-6 col-xxl-3">
-            <div className="d-flex align-items-center justify-content-between statistics_funfact">
-              <div className="details">
-                <div className="fz15">Αξιολογήσεις</div>
-                <div className="title">{totalReviews}</div>
-                {/* <div className="text fz14">
-                  <span className="text-thm">290+</span> New Review
-                </div> */}
-              </div>
-              <div className="icon text-center">
-                <i className="flaticon-review-1" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-6 col-xxl-4">
-            <div className="ps-widget bgc-white bdrs4 p30 mb30 overflow-hidden position-relative">
-              <div className="d-flex justify-content-between bdrb1 pb15 mb20">
-                <h5 className="title">Δημοφιλείς Υπηρεσίες</h5>
-                <Link
-                  href="/dashboard/services"
-                  className="text-decoration-underline text-thm6"
-                >
-                  Περισσότερα
-                </Link>
-              </div>
-              {popularServices.services.data.length > 0 ? (
-                <div className="dashboard-img-service">
-                  {popularServices.services.data.map((item, i) => (
-                    <div key={i}>
-                      <MostViewServiceCard1 data={item} />
-                    </div>
-                  ))}
-                </div>
+              {hasAccess ? (
+                <h2>Πίνακας Ελέγχου</h2>
               ) : (
-                <div className="dashboard-img-service">
-                  <p>Δεν βρέθηκαν δημοφιλέις υπηρεσίες</p>
-                </div>
+                <h2>Καλωσόρισες, {user.displayName}!</h2>
               )}
             </div>
           </div>
-          <div className="col-md-6 col-xxl-4">
-            <div className="ps-widget bgc-white bdrs4 p30 mb30 overflow-hidden position-relative">
-              <div className="d-flex justify-content-between bdrb1 pb15 mb30">
-                <h5 className="title">Τελευταία Μηνύματα</h5>
-                <a className="text-decoration-underline text-thm6">
-                  Περισσότερα
-                </a>
+        </div>
+        {hasAccess ? (
+          <>
+            <div className="row">
+              <div className="col-sm-6 col-xxl-3">
+                <div className="d-flex align-items-center justify-content-between statistics_funfact">
+                  <div className="details">
+                    <div className="fz15">Υπηρεσίες</div>
+                    <div className="title">{data?.totalServices}</div>
+                    {/* <div className="text fz14">
+                  <span className="text-thm">10</span> New Offered
+                </div> */}
+                  </div>
+                  <div className="icon text-center">
+                    <i className="flaticon-contract" />
+                  </div>
+                </div>
               </div>
-              <div className="dashboard-img-service">
-                {job1.slice(0, 3).map((item, i) => (
+              <div className="col-sm-6 col-xxl-3">
+                <div className="d-flex align-items-center justify-content-between statistics_funfact">
+                  <div className="details">
+                    <div className="fz15">Αξιολογήσεις</div>
+                    <div className="title">{data?.totalReviews}</div>
+                    {/* <div className="text fz14">
+                  <span className="text-thm">290+</span> New Review
+                </div> */}
+                  </div>
+                  <div className="icon text-center">
+                    <i className="flaticon-review-1" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6 col-xxl-4">
+                <div className="ps-widget bgc-white bdrs4 p30 mb30 overflow-hidden position-relative">
+                  <div className="d-flex justify-content-between bdrb1 pb15 mb20">
+                    <h5 className="title">Δημοφιλείς Υπηρεσίες</h5>
+                    <Link
+                      href="/dashboard/services"
+                      className="text-decoration-underline text-thm6"
+                    >
+                      Περισσότερα
+                    </Link>
+                  </div>
+                  {data?.popularServices.services.data.length > 0 ? (
+                    <div className="dashboard-img-service">
+                      {data?.popularServices.services.data.map((item, i) => (
+                        <div key={i}>
+                          <MostViewServiceCard1 data={item} />
+                        </div>
+                      ))}
+                    </div>
+                  ) : (
+                    <div className="dashboard-img-service">
+                      <p>Δεν βρέθηκαν δημοφιλέις υπηρεσίες</p>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="col-md-6 col-xxl-4">
+                <div className="ps-widget bgc-white bdrs4 p30 mb30 overflow-hidden position-relative">
+                  <div className="d-flex justify-content-between bdrb1 pb15 mb30">
+                    <h5 className="title">Τελευταία Μηνύματα</h5>
+                    <a className="text-decoration-underline text-thm6">
+                      Περισσότερα
+                    </a>
+                  </div>
+                  <div className="dashboard-img-service">
+                    {/* {job1.slice(0, 3).map((item, i) => (
                   <div key={i}>
                     <RecentServiceCard1 data={item} />
                     {product1.slice(0, 3).length !== i + 1 && (
                       <hr className="opacity-100 mt-0" />
                     )}
                   </div>
-                ))}
+                ))} */}
+                  </div>
+                </div>
               </div>
             </div>
+          </>
+        ) : (
+          <div>
+            Συμπλήρωσε τα στοιχεία σου στην{" "}
+            <strong>
+              <Link href="/dashboard/profile">Διαχείρηση Προφίλ </Link>
+            </strong>
+            για να αποκτήσεις πρόσβαση σε περισσότερες λειτουργίες
           </div>
-        </div>
+        )}
       </div>
     </>
   );
