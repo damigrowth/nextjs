6b7143ad5803f3dab948318a3697a0e7c9b5ec0a
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2025-01-31 20:49:29 +0200

    ML6-133 Email Verification - Auth & Dashboard

---
+++ src/app/(auth)/email-confirmation/page.jsx
@@ -0,0 +1,31 @@
+import EmailConfirmationForm from "@/components/ui/forms/EmailConfirmationForm";
+import { getToken } from "@/lib/auth/token";
+import { getUser } from "@/lib/auth/user";
+
+export default async function page({ searchParams }) {
+  const confirmationCode = searchParams?.code || "";
+
+  return (
+    <section className="our-register">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 m-auto wow fadeInUp" data-wow-delay="300ms">
+            <div className="main-title text-center">
+              <h2 className="title">Επιβεβαίωση της διεύθυνσής σας email</h2>
+              <p className="paragraph">
+                Παρακαλώ περιμένετε όσο επιβεβαιώνουμε το email σας...
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="row wow fadeInRight" data-wow-delay="300ms">
+          <div className="col-xl-6 mx-auto">
+            <div className="log-reg-form search-modal form-style1 bgc-white p50 p30-sm default-box-shadow1 bdrs12">
+              <EmailConfirmationForm confirmationCode={confirmationCode} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
