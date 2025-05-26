a1e06481db86db691e77d6c2ce1394ca0fb3aa91
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2025-05-26 22:10:46 +0300

    Fixing serialized strings of Sentry and removed comps

--- src/components/archives-services-price.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Slider from "./range-slider";
-
-export default function Price() {
-  return <Slider iniMin={5} iniMax={1000} />;
-}
--- src/components/archives-services-time.jsx
+++
@@ -1,6 +0,0 @@
-import RadioBox from "./ui/Archives/Inputs/RadioBox";
-import { serviceTimeOptions } from "./ui/Archives/options";
-
-export default function Time() {
-  return <RadioBox options={serviceTimeOptions} paramName="time" />;
-}
