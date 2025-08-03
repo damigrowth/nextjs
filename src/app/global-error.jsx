021006666715455e4c67e9f03b4002307957efd5
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2025-08-04 00:45:57 +0300

    Update global-error.jsx

--- src/app/global-error.jsx
+++ src/app/global-error.jsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import NextError from 'next/error';
-import * as Sentry from '@sentry/nextjs';
+// import * as Sentry from '@sentry/nextjs';
 
 export default function GlobalError({ error }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
+  // useEffect(() => {
+  //   Sentry.captureException(error);
+  // }, [error]);
 
   return (
     <html>
