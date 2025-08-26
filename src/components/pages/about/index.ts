afebe7f0bf966a716dc7028557bc31a3d0237988
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2025-08-26 14:34:40 +0300

    Remove legacy page components and JS email templates
    - Remove about page specific components in favor of shared components
    - Delete old JavaScript email templates replaced by TypeScript versions
    - Clean up component structure for better maintainability
    - Consolidate component organization for the new architecture

--- src/components/pages/about/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as BannerAbout } from './about-banner';
-export { default as FeaturesCounterAbout } from './about-features-counter';
-export { default as FeaturesAbout } from './about-features';
-export { default as FeaturesStatsAbout } from './about-features-stats';
-export { default as TestimonialsAbout } from './about-testimonials';
-export { default as FeaturesImageAbout } from './about-features-image';
-export { default as FaqAbout } from './about-faq';
--- src/constants/email/templates/index.js
+++
@@ -1,9 +0,0 @@
-import { VERIFICATION } from './verification';
-import { WELCOME } from './welcome';
-import { PASSWORD_RESET } from './password-reset';
-
-export const EMAIL_TEMPLATES = {
-  VERIFICATION,
-  WELCOME,
-  PASSWORD_RESET,
-};
