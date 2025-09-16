50a2840ebdf6af737ea5153cd3c881d69716bbd7
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2025-09-17 02:12:24 +0300

    Add service edit form exports
    - Export FormServiceEdit for service editing functionality
    - Export FormServiceEditMedia for media-specific editing
    - Expand service form component exports for comprehensive editing

--- src/components/forms/service/index.ts
+++ src/components/forms/service/index.ts
@@ -1,2 +1,4 @@
 export * from './steps';
 export { default as FormCreateService } from './form-service-create';
+export { default as FormServiceEdit } from './form-service-edit';
+export { default as FormServiceEditMedia } from './form-service-edit-media';
