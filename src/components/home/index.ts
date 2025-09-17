dc74836b8b571ea4ba706913d36befec1f144619
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2025-09-17 17:27:59 +0300

    Update home components exports
    Replace FreelancersHome export with ProfilesHome to reflect
    the component rename and modernization effort.

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -2,9 +2,9 @@
 
 export { default as CategoriesHome } from './home-categories';
 export { default as FeaturesHome } from './home-features';
-export { default as FreelancersHome } from './home-freelancers';
 export { default as HeroHome } from './home-hero';
 export { default as ServicesHome } from './home-services';
+export { default as ProfilesHome } from './home-profiles';
 export { default as TaxonomiesHome } from './home-taxonomies';
 export { default as TestimonialsHome } from './home-testimonials';
 export { HomeSearch } from './home-search';
