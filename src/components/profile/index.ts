1d5e28067f58113981079dd8e3b4c99a5f6ee3fd
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2025-09-18 01:34:15 +0300

    Export ProfileServices component from profile components index
    - Add ProfileServices to centralized component exports
    - Enables clean imports throughout the application

--- src/components/profile/index.ts
+++ src/components/profile/index.ts
@@ -5,6 +5,7 @@ export { default as ProfileBio } from './profile-bio';
 export { default as ProfileFeatures } from './profile-features';
 export { default as ProfileIndustries } from './profile-industries';
 export { default as ProfilePortfolio } from './profile-portfolio';
+export { default as ProfileServices } from './profile-services';
 export { default as ProfileTerms } from './profile-terms';
 export { default as ProfileInfo } from './profile-info';
 export { default as ProfileSkills } from './profile-skills';
