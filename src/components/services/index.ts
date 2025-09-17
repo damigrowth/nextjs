234426dae3a6d9efcd64bc0863bfa9c47e0a296f
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2025-09-17 17:28:16 +0300

    Remove legacy services component index
    Remove legacy services components index file as part of
    component restructuring and modernization effort.

--- src/components/services/index.ts
+++
@@ -1,5 +0,0 @@
-// Services Components
-// export * from '../forms/service/steps';
-// export { default as FormCreateService } from '../forms/service/form-service-create';
-// export { default as TaxonomySelector } from '../shared/taxonomy-selector';
-// export { default as RatingDisplay } from '../shared/rating-display';
