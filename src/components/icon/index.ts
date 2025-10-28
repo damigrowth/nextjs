54d172e338e2f4952d3dde5e9f499462b2f21888
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2025-10-28 15:40:32 +0200

    ML14-65: Migrate icon components to TypeScript
    Convert legacy JSX icon components to TypeScript TSX format
    for type safety and better IDE support.
    
    Jira: ML14-65

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,4 +1,6 @@
 export { default as IconPulsatingDot } from './icon-pulsating-dot';
 export { default as Socials } from './socials';
-export { default as Spinner } from './spinner';
 export { default as IconMobileMenu } from './icon-mobile-menu';
+
+// Flaticon icons
+export { FlaticonCategory } from './flaticon';
