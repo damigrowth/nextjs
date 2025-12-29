55b764717027fa86eb8377943fc44f8c281db19d
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2025-12-29 23:00:43 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // TEMPORARY: Disable strict mode to test if React double-mounting is causing realtime subscription issues
@@ -178,4 +174,4 @@ const nextConfig = {
   }),
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
