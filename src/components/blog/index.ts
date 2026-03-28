a9fc350450246229c92aafc460035ab64bf35c5f
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2026-03-28 20:03:57 +0200

    ML16-39 Blog UI: remove unused BlogCategoryTabs component

--- src/components/blog/index.ts
+++ src/components/blog/index.ts
@@ -3,7 +3,6 @@ export { default as ArticleHeader } from './article-header';
 export { default as ArticleContent } from './article-content';
 export { default as ArticleToc } from './article-toc';
 export { default as AuthorBox } from './author-box';
-export { default as BlogCategoryTabs } from './blog-category-tabs';
 export { default as BlogPagination } from './blog-pagination';
 export { default as FeaturedArticleHero } from './featured-article-hero';
 export { default as CompactArticleRow } from './compact-article-row';
